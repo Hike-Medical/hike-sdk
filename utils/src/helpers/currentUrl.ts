0267bc32be49b94916bc2b07a0ab33e6288f6b51
Basem Hike <basem@hikemedical.com>
2024-04-28 14:16:40 +0400

    Handle undefined `window.location` in React Native

--- utils/src/helpers/currentUrl.ts
+++ utils/src/helpers/currentUrl.ts
@@ -5,7 +5,7 @@ import { toURL } from '../converters/toURL';
  * If `window` is not defined due to server-side execution, this function will return `null`.
  */
 export const currentUrl = (params?: Record<string, string | null | undefined>) => {
-  const url = typeof window !== 'undefined' ? toURL(window.location.href) : null;
+  const url = typeof window !== 'undefined' ? toURL(window.location?.href) : null;
 
   if (!url) {
     return null;
