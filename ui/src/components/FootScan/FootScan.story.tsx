3a52d719beebd465803ee528059ca0208d061be9
Amrith Balachander <amrith@hikemedical.com>
2024-05-01 10:05:14 -0500

    updated clinical scans commit

---
+++ ui/src/components/FootScan/FootScan.story.tsx
@@ -0,0 +1,9 @@
+import { Side } from '@prisma/client';
+import React from 'react';
+import FootScan, { FootScanProps } from './FootScan';
+
+export default { component: FootScan, 
+  args: { side: Side.LEFT, status: 'NOT_STARTED' } as FootScanProps }; 
+
+
+export const DefaultLeft = (args: FootScanProps) => React.createElement(FootScan, args);
