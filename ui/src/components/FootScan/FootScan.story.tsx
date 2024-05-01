7e3ee25728634541f759f195f3646268472c8780
Amrith Balachander <amrith@hikemedical.com>
2024-05-01 17:39:58 -0500

    updated clinical foot scan ui

--- ui/src/components/FootScan/FootScan.story.tsx
+++ ui/src/components/FootScan/FootScan.story.tsx
@@ -1,9 +1,38 @@
 import { Side } from '@prisma/client';
-import React from 'react';
+import React, { useState } from 'react';
 import FootScan, { FootScanProps } from './FootScan';
 
-export default { component: FootScan, 
-  args: { side: Side.LEFT, status: 'NOT_STARTED' } as FootScanProps }; 
+const FootScanContainer = (args: FootScanProps) => {
+  const [isSwitchOn, setIsSwitchOn] = useState(true);
+  console.log(React)
 
+  return (
+    <FootScan
+      {...args}
+      setIsSwitchOn={setIsSwitchOn}
+      isSwitchOn={isSwitchOn}
+    />
+  );
+};
 
-export const DefaultLeft = (args: FootScanProps) => React.createElement(FootScan, args);
+export default {
+  component: FootScanContainer,
+  args: {
+    side: Side.LEFT,
+    status: 'NOT_STARTED',
+  } as FootScanProps,
+  argTypes: {
+    status: {
+      options: ['NOT_STARTED', 'APPROVED', 'REJECTED',  'PROCESSING'],
+      control: { type: 'radio' },
+    },
+    side: {
+      options: [Side.LEFT, Side.RIGHT],
+      control: { type: 'radio' },
+    }
+  },
+};
+
+export const DefaultLeft = (args: FootScanProps) => (
+  <FootScanContainer {...args} />
+);
--- ui/src/components/FootScan/FootScan.tsx
+++ ui/src/components/FootScan/FootScan.tsx
@@ -1,24 +1,39 @@
 import { toTitleCase } from '@hike/utils';
-import { ActionIcon, Box, Button, Group, Paper, Switch, Text, rem } from '@mantine/core';
+import { ActionIcon, Box, Button, Group, Paper, Pill, Stack, Switch, Text, rem } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Side } from '@prisma/client';
-import { IconChevronUp, IconCircle } from '@tabler/icons-react';
+import { IconChevronUp, IconCircle, IconUpload } from '@tabler/icons-react';
 import React from 'react';
+
+export type FootScanStatus = 'NOT_STARTED' | 'APPROVED' | 'REJECTED' | 'PROCESSING';
 
 export interface FootScanProps {
   side: Side
-  status: 'NOT_STARTED' | 'APPROVED' | 'REJECTED' | 'PROCESSING' | 'DISABLED';
+  status: FootScanStatus;
+  isSwitchOn: boolean;
+  setIsSwitchOn: (isDisabled: boolean) => void;
 }
 
 export default function FootScan({
     side,
-    status
+    status,
+    isSwitchOn,
+    setIsSwitchOn
 }: FootScanProps) {
 
     console.log(React)
 const [moreOptionsOpen, { toggle: toggleExpand }] = useDisclosure(true);
 
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsSwitchOn(event.currentTarget.checked);
+  };
+
+
 const getIconStatusColor = () => {
+    if(!isSwitchOn) {
+        return '#868E96'
+    }
     switch (status) {
         case 'NOT_STARTED':
         return '#0088EA';
@@ -28,59 +43,141 @@ const getIconStatusColor = () => {
         return '#FF8787';
         case 'PROCESSING':
         return '#FFF3BF';
-        case 'DISABLED':
-        return '#868E96';
         default:
         return 'white';
+    }
+};
+
+const getStartText = () => {
+    switch (status) {
+        case 'NOT_STARTED':
+        return 'Start Recording';
+        case 'APPROVED':
+        return 'Video Uploaded';
+        case 'REJECTED':
+        return 'Rescan Required';
+        case 'PROCESSING':
+        return 'Video Processing';
+        default:
+        return 'No Action Required';
     }
 };
 
   return (
     <Paper shadow="md" p="md" style={{ maxWidth: 400 }}>
       <Box mb={20}>
-        <Group justify='space-between'>
+
+      {!isSwitchOn && (
+        <Pill
+            bg="black"
+            styles={{
+            label: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            }}
+        >
+            <Text c="white" size="12px" fw={700}>
+            Skipped
+            </Text>
+        </Pill>
+        )}
+
+        <Group justify='space-between' mt={8}>
         <Group gap='xs'>
         <IconCircle size = '16' color={getIconStatusColor()} fill={getIconStatusColor()}/>
-          <Text>{toTitleCase(side)} foot</Text>
+          <Text>{toTitleCase(side)} Foot</Text>
           </Group>
-          <Switch size="md" color='green.4'/>
+          <Switch size="md" color="green.4" checked={isSwitchOn} onChange={handleSwitchChange} />
         </Group>
       </Box>
+  
 
-      <Button fullWidth color={getIconStatusColor()} mb={20}>
-        Start Recording
-      </Button>
-      <Box>
-        <Group justify='center'>
-            <Text size="sm" c="black">
-                    More Options
-            </Text>
-            <ActionIcon
-              data-automation-id="more-details-toggle-button"
-              radius="100%"
-              variant="filled"
-              aria-label="Collapse Toggle"
-              styles={{
+        {isSwitchOn && <>
+            <Button
+            fullWidth
+            color={getIconStatusColor()}
+            mb={20}
+            styles={{
                 root: {
-                  background: '#FFFFFF90'
-                }
-              }}
-              onClick={toggleExpand}
+                color: status === 'PROCESSING' ? 'black' : 'white',
+                height: '56px',
+                fontSize: '18px', 
+                borderRadius: '12px', 
+                },
+            }}
             >
-              <IconChevronUp
-                color="black"
-                style={{
-                  width: rem(18),
-                  height: rem(18),
-                  transform: moreOptionsOpen ? 'rotate(0deg)' : 'rotate(180deg)',
-                  transition: 'transform 0.2s ease-in-out'
-                }}
-                stroke={1.5}
-              />
-            </ActionIcon>
-            </Group>
-
+            {getStartText()}
+            </Button>
+      <Box>
+      <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Text size="sm" c="black"  onClick={toggleExpand}>
+            More Options
+        </Text>
+        <ActionIcon
+            data-automation-id="more-details-toggle-button"
+            radius="100%"
+            variant="filled"
+            aria-label="Collapse Toggle"
+            styles={{ root: { background: '#FFFFFF90' } }}
+            onClick={toggleExpand}
+        >
+            <IconChevronUp
+            color="black"
+            style={{
+                width: rem(18),
+                height: rem(18),
+                transform: moreOptionsOpen ? 'rotate(0deg)' : 'rotate(180deg)',
+                transition: 'transform 0.2s ease-in-out',
+            }}
+            stroke={1.5}
+            />
+        </ActionIcon>
+        </Box>
       </Box>
+
+      { moreOptionsOpen && <Stack mt={20}>
+
+            <Button
+                fullWidth
+                styles={{
+                    root: {
+                    height: '48px',
+                    fontSize: '16px',
+                    borderRadius: '12px',
+                    backgroundColor: 'black', 
+                    color: 'white', 
+                    },
+                }}
+                >
+            <Group>
+             <IconUpload size = '24' color='white'/>
+                Upload Video
+            </Group>
+                </Button>
+                
+                <Button
+                fullWidth
+                variant="outline" 
+                styles={{
+                    root: {
+                    height: '48px',
+                    fontSize: '16px',
+                    borderRadius: '12px',
+                    borderColor: 'gray', 
+                    backgroundColor: 'white',
+                    color: 'gray', 
+                    },
+                }}
+                >
+                Upload Impression Box Scans
+                </Button>
+      
+        </Stack>
+      }
+      </>}
+
     </Paper>
   );
 }
