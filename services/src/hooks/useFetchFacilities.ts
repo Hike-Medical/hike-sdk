f4a0bd902513c79789c5538c094f6ccef918b67a
nvarsh-hm <nik@hikemedical.com>
2024-05-12 19:40:27 -0400

    fix: rename fn

--- services/src/hooks/useFetchFacilities.ts
+++ services/src/hooks/useFetchFacilities.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchCompanyFacilitiesAndAddresses } from '../api/shipping.service';
 
-export const useGetFacilities = () => {
+export const useFetchFacilities = () => {
   return useQuery({
     queryKey: ['facilities'],
     queryFn: async () => fetchCompanyFacilitiesAndAddresses()
