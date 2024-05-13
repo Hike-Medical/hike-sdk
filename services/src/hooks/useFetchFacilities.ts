bb9f6e7faa724b57da13607b4a9d54a87ea3ebf6
nvarsh-hm <nik@hikemedical.com>
2024-05-13 10:40:52 -0400

    chore: add bill recipient

--- services/src/hooks/useFetchFacilities.ts
+++ services/src/hooks/useFetchFacilities.ts
@@ -2,7 +2,9 @@ import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 import { fetchCompanyFacilitiesAndAddresses } from '../api/facility.service';
 import { Facility, FacilityAddress } from '@hike/types';
 
-export const useFetchFacilities = (options?: UseQueryOptions<Facility & { address: FacilityAddress }[], Error>) => {
+export const useFetchFacilities = (
+  options?: Omit<UseQueryOptions<Facility & { address: FacilityAddress }[], Error>, 'queryKey' | 'queryFn'>
+) => {
   return useQuery({
     queryKey: ['facilities'],
     queryFn: async () => fetchCompanyFacilitiesAndAddresses(),
