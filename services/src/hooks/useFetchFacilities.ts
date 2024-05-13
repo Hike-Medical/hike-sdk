a7f2d66e170ca5accbfd4002da36dcce1ce00db3
nvarsh-hm <nik@hikemedical.com>
2024-05-12 20:15:36 -0400

    chore: add types

--- services/src/hooks/useFetchFacilities.ts
+++ services/src/hooks/useFetchFacilities.ts
@@ -1,9 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 import { fetchCompanyFacilitiesAndAddresses } from '../api/facility.service';
+import { Facility, FacilityAddress } from '@hike/types';
 
-export const useFetchFacilities = () => {
+export const useFetchFacilities = (options?: UseQueryOptions<Facility & { address: FacilityAddress }[], Error>) => {
   return useQuery({
     queryKey: ['facilities'],
-    queryFn: async () => fetchCompanyFacilitiesAndAddresses()
+    queryFn: async () => fetchCompanyFacilitiesAndAddresses(),
+    ...options
   });
 };
