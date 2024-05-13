d8190211aaeb62a58d8c47e7529e4d62c671d4f3
Basem <basem@hikemedical.com>
2024-05-13 17:33:26 +0400

    Dynamic form updates and fixes (#24)
    * Fix backend host for mobile
    
    * Time zone support
    
    * Minor test fixes
    
    * Optional time zone fix
    
    * Update database schema
    
    * Fix start UTC date calculation
    
    * Update field type for date
    
    * Add gender select field
    
    * Add device type field
    
    * Add more select field types
    
    * Update multiselect properties
    
    * Update field text properties
    
    * Update form key

--- types/src/entities/FormSchema.ts
+++ types/src/entities/FormSchema.ts
@@ -24,17 +24,24 @@ interface BaseFormField<T extends FormFieldValue> {
   print?: boolean | { label: string };
   default?: T;
   rule?: {
-    effect: 'SHOW' | 'HIDE' | 'ENABLE' | 'DISABLE';
-    condition: { name: string; value: FormFieldValue | FormFieldValue[] };
+    effect: 'show' | 'hide' | 'enable' | 'disable';
+    condition: { name: string; value: FormFieldValue };
   };
 }
 
 export type FormField =
-  | (BaseFormField<string> & { type: 'text'; multi?: boolean })
+  | (BaseFormField<string> & { type: 'text' })
   | (BaseFormField<number> & { type: 'number' })
   | (BaseFormField<boolean> & { type: 'boolean' })
-  | (BaseFormField<string> & { type: 'date'; display?: string })
+  | (BaseFormField<string> & { type: 'date'; mode?: 'date' | 'datetime' | 'time'; min?: string; max?: string })
   | (BaseFormField<string> & { type: 'address' })
   | (BaseFormField<string> & { type: 'image' })
   | (BaseFormField<string> & { type: 'select'; options: { label: string; value: string }[] })
-  | (BaseFormField<string[]> & { type: 'multiselect'; options: { label: string; value: string }[] });
+  | (BaseFormField<string> & { type: 'select:gender' })
+  | (BaseFormField<string> & { type: 'select:height' })
+  | (BaseFormField<string> & { type: 'select:weight' })
+  | (BaseFormField<string> & { type: 'select:device-type' })
+  | (BaseFormField<string> & { type: 'select:physician' })
+  | (BaseFormField<string> & { type: 'select:diagnosis' })
+  | (BaseFormField<string> & { type: 'select:patient' })
+  | (BaseFormField<string[]> & { type: 'multiselect'; options: { label: string; value: string }[]; title?: string });
