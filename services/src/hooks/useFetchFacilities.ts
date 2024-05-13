c6275cced64ef29383374584efcf64b9d71ee31f
nvarsh-hm <nik@hikemedical.com>
2024-05-13 11:37:15 -0400

    feat: update TS response interface

--- services/src/hooks/useFetchFacilities.ts
+++ services/src/hooks/useFetchFacilities.ts
@@ -3,7 +3,7 @@ import { fetchCompanyFacilitiesAndAddresses } from '../api/facility.service';
 import { Facility, FacilityAddress } from '@hike/types';
 
 export const useFetchFacilities = (
-  options?: Omit<UseQueryOptions<Facility & { address: FacilityAddress }[], Error>, 'queryKey' | 'queryFn'>
+  options?: Omit<UseQueryOptions<(Facility & { address: FacilityAddress })[], Error>, 'queryKey' | 'queryFn'>
 ) => {
   return useQuery({
     queryKey: ['facilities'],
