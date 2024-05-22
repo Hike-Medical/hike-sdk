31316d0666340fd4ee3499354ca9d965ec66309a
Basem Hike <basem@hikemedical.com>
2024-05-22 21:53:48 +0400

    Evaluation form updates

---
+++ services/src/hooks/useFormSubmission.ts
@@ -0,0 +1,26 @@
+import type { FormSubmissionExtended } from '@hike/types';
+import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { findFormSubmission } from '../api/form.service';
+import { ResponseError } from '../errors/ResponseError';
+
+export interface UseFormSubmissionOptions
+  extends Omit<UseQueryOptions<FormSubmissionExtended | null, ResponseError<null>>, 'queryKey' | 'queryFn'> {
+  templateId: string;
+  workbenchId: string;
+  queryKey?: QueryKey;
+}
+
+export const useFormSubmission = ({ templateId, workbenchId, queryKey = [], ...options }: UseFormSubmissionOptions) => {
+  const key = ['formSubmission', templateId, workbenchId, ...queryKey];
+
+  const query = useQuery({
+    queryKey: key,
+    queryFn: async () => await findFormSubmission(templateId, workbenchId),
+    ...options
+  });
+
+  return {
+    queryKey: key,
+    ...query
+  };
+};
