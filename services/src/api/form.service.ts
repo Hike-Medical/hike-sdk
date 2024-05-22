31e9e5923076c6f32c677159f145483194054255
Basem <basem@hikemedical.com>
2024-05-22 23:22:57 +0400

    Evaluation form updates (#48)
    * Evaluation form updates
    
    * Add device side and position

--- services/src/api/form.service.ts
+++ services/src/api/form.service.ts
@@ -11,6 +11,14 @@ export const findFormTemplatesByIds = async (templateIds: string[]): Promise<For
   return response.data;
 };
 
+export const findFormSubmission = async (
+  templateId: string,
+  workbenchId: string
+): Promise<FormSubmissionExtended | null> => {
+  const response = await backendApi.get(`form/template/${templateId}/workbench/${workbenchId}/submission`);
+  return response.data;
+};
+
 export const findFormSubmissionsByWorkbenchId = async (workbenchId: string): Promise<FormSubmissionExtended[]> => {
   const response = await backendApi.get(`form/workbench/${workbenchId}/submission`);
   return response.data;
