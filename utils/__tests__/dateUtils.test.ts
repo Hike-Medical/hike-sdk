b465d637690e750aa42ece31b055a9edff937b44
Basem <basem@hikemedical.com>
2024-05-30 18:30:18 +0400

    Form utils additions and tests (#65)

--- utils/__tests__/dateUtils.test.ts
+++ utils/__tests__/dateUtils.test.ts
@@ -67,49 +67,49 @@ describe('dateUtils', () => {
     test('converts UTC date to the start of the day in local time correctly', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-10-15T00:00:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('10-15-2023 12:00 AM');
     });
 
     test('converts local time in the middle of the day back to the start of the day in UTC correctly', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-10-16T00:00:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('10-16-2023 12:00 AM');
     });
 
     test('converts time just before local midnight to the start of the same day in local time', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-10-16T03:59:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('10-16-2023 12:00 AM');
     });
 
     test('converts time just before UTC midnight to the start of the same day in local time', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-10-16T23:59:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('10-16-2023 12:00 AM');
     });
 
     test('handles time just after midnight to ensure it remains the same day in local time', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-10-15T04:01:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('10-15-2023 12:00 AM');
     });
 
     test('accounts for daylight saving time changes when rolling back', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-11-05T05:00:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('11-05-2023 12:00 AM');
     });
 
     test('accounts for daylight saving time changes when rolling forward', () => {
       const timeZone = 'America/New_York';
       const date = new Date('2023-11-04T04:00:00Z');
-      const localDate = fromStartOfUTC(date, timeZone);
+      const localDate = fromStartOfUTC(date, dayjs().tz(timeZone).utcOffset() * 60000);
       expect(dayjs.tz(localDate, timeZone).format('MM-DD-YYYY hh:mm A')).toEqual('11-04-2023 12:00 AM');
     });
 
