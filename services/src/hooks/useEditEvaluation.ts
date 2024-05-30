ef176f1e3b56721f631ae21babccb337384f20a7
Amrith Balachander <amrith@hikemedical.com>
2024-05-30 18:33:43 -0500

    updated hooks

---
+++ services/src/hooks/useEditEvaluation.ts
@@ -0,0 +1,13 @@
+import { ActionEvaluationParams, EvaluationExtended } from '@hike/types';
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+import { editEvaluation } from '../api/evaluation.service';
+
+export const useEditEvaluation = (
+  mutationOptions?: UseMutationOptions<EvaluationExtended, Error, ActionEvaluationParams>
+) => {
+  return useMutation({
+    mutationKey: ['editEvaluation'],
+    mutationFn: async (body: ActionEvaluationParams) => await editEvaluation(body),
+    ...mutationOptions
+  });
+};
