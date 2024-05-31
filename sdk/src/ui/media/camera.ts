f066f60c9bc9a6dfa1154520bdbe2b3a97d72dc4
Basem Hike <basem@hikemedical.com>
2024-05-31 10:13:13 +0400

    Add jsdocs comments

--- sdk/src/ui/media/camera.ts
+++ sdk/src/ui/media/camera.ts
@@ -1,3 +1,10 @@
+/**
+ * Requests camera permissions from the user.
+ * If permissions are granted, it resolves with a MediaStream object.
+ * If permissions are denied, it rejects with an error message.
+ *
+ * @returns A promise that resolves with the media stream if permissions are granted.
+ */
 export const requestCameraPermissions = (): Promise<MediaStream> =>
   new Promise((resolve, reject) => {
     navigator.mediaDevices
--- sdk/src/ui/media/getBestSupportedVideoFormat.ts
+++ sdk/src/ui/media/getBestSupportedVideoFormat.ts
@@ -1,5 +1,11 @@
 import { getSupportedMimeTypes } from './getSupportedMimeTypes';
 
+/**
+ * Determines the best supported video format based on a predefined list of video types and codecs.
+ * The function returns the first supported format found. If no supported format is found, it defaults to 'video/mp4'.
+ *
+ * @returns The best supported video format or 'video/mp4' if none of the formats are supported.
+ */
 export const getBestSupportedVideoFormat = (): string => {
   const videoTypes = ['webm', 'ogg', 'mp4', 'x-matroska'];
   const codecs = [
