080bd303e7cc27ca954f1b682e3fbf4841c3aac8
nvarsh-hm <nik@hikemedical.com>
2024-05-30 19:56:42 -0500

    chore: remove workbench-service

--- services/src/api/workbench-service.ts
+++
@@ -1,6 +0,0 @@
-import { backendApi } from '../utils/backendApi';
-
-export const submitWorkbench = async (workbenchId: string) => {
-  const response = await backendApi.post(`workbench/${workbenchId}/submit`);
-  return response.data;
-};
