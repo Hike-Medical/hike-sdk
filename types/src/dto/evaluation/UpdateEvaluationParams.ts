86f575602cf59721ed12974df3bbabbcb7fb051e
Basem Hike <basem@hikemedical.com>
2024-06-11 22:05:46 +0400

    Updates to evaluation dto

--- types/src/dto/evaluation/UpdateEvaluationParams.ts
+++ types/src/dto/evaluation/UpdateEvaluationParams.ts
@@ -3,4 +3,7 @@ import { CreateEvaluationParams } from './CreateEvaluationParams';
 /**
  * Data transfer object for updating an evaluation.
  */
-export interface UpdateEvaluationParams extends Partial<CreateEvaluationParams> {}
+export interface UpdateEvaluationParams extends Partial<CreateEvaluationParams> {
+  submittedAt?: Date;
+  cancelledAt?: Date;
+}
