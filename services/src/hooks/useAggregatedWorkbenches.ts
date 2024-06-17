397e8eedcc584a3a925c093e728e012ac0863018
nvarsh-hm <nik@hikemedical.com>
2024-06-17 10:15:30 -0500

    chore: fix params

--- services/src/hooks/useAggregatedWorkbenches.ts
+++ services/src/hooks/useAggregatedWorkbenches.ts
@@ -1,14 +1,16 @@
 import { AggregatedWorkbenchResponse, GetAggregatedParams } from '@hike/types';
 import { ResponseError } from '../errors/ResponseError';
 import { getAggregatedWorkbenches } from '../api/workbench.service';
-import { useQuery } from '@tanstack/react-query';
+import { UseQueryOptions, useQuery } from '@tanstack/react-query';
 
-export interface UseAggregatedWorkbenchOptions extends GetAggregatedParams {
+export interface UseAggregatedWorkbenchOptions
+  extends Omit<UseQueryOptions<AggregatedWorkbenchResponse, ResponseError<null>>, 'queryFn' | 'queryKey'> {
+  params?: GetAggregatedParams;
   key?: string[];
   enabled?: boolean;
 }
 
-export const useAggregatedWorkbenches = ({ key = [], enabled = true, ...params }: UseAggregatedWorkbenchOptions) => {
+export const useAggregatedWorkbenches = ({ key = [], enabled = true, params }: UseAggregatedWorkbenchOptions) => {
   return useQuery<AggregatedWorkbenchResponse, ResponseError<null>>({
     queryKey: ['useAggregatedWorkbenches', ...key, params],
     queryFn: async () => await getAggregatedWorkbenches(params),
