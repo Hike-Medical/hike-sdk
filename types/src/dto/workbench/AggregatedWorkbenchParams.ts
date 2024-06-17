313f8b3b99656b48fb39262e67a6b5010d0f7b0f
nvarsh-hm <nik@hikemedical.com>
2024-06-17 17:05:15 -0500

    chore: add more params

--- types/src/dto/workbench/AggregatedWorkbenchParams.ts
+++ types/src/dto/workbench/AggregatedWorkbenchParams.ts
@@ -3,4 +3,8 @@ import { PagedParams } from '../../dto/PagedParams';
 export interface GetAggregatedParams extends PagedParams {
   orderStatus?: string;
   validationStatus?: boolean | null;
+  diabeticOnly?: boolean;
+  authorizationStartDate?: string;
+  authorizationEndDate?: string;
+  searchQuery?: string;
 }
