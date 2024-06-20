35db2feb3387871db401ff570e982ac221dafbc9
nvarsh-hm <154340007+nvarsh-hm@users.noreply.github.com>
2024-06-19 21:11:38 -0500

    feat: add pin to user (#130)
    * feat: add pin to user
    
    * chore: add PIN constant

--- utils/src/constants.ts
+++ utils/src/constants.ts
@@ -2,6 +2,7 @@ import type { PaginationState } from '@hike/types';
 
 export const Constants = {
   AUTH_COOKIE_NAME: 'hike.session-token',
+  MIN_PIN_LENGTH: 8,
   DEFAULT_QUERY_LIMIT: 1000,
   DEFAULT_PAGINATION: {
     pageIndex: 0,
