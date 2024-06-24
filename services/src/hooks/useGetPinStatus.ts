3bb0268aa83415e4cc7bd6792d4949f49fe15a3c
nvarsh-hm <nik@hikemedical.com>
2024-06-24 10:39:49 -0400

    chore: make pin status param optional

--- services/src/hooks/useGetPinStatus.ts
+++ services/src/hooks/useGetPinStatus.ts
@@ -3,7 +3,7 @@ import { getPinStatus } from '../api/pin.service';
 import { ResponseError } from 'errors/ResponseError';
 
 export const useGetPinStatus = (
-  options: Omit<UseQueryOptions<boolean, ResponseError<null>>, 'queryKey' | 'queryFn'>
+  options?: Omit<UseQueryOptions<boolean, ResponseError<null>>, 'queryKey' | 'queryFn'>
 ) => {
   return useQuery({
     queryKey: ['pinStatus'],
