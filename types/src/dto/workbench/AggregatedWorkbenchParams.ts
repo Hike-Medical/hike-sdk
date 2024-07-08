54f8c344c6a4b37a3208a0e348c68202360665c7
Amrith Balachander <amrith@hikemedical.com>
2024-07-08 17:17:22 -0500

    add submittedOnly and devValidatoinStatus

--- types/src/dto/workbench/AggregatedWorkbenchParams.ts
+++ types/src/dto/workbench/AggregatedWorkbenchParams.ts
@@ -3,8 +3,10 @@ import { PagedParams } from '../../dto/PagedParams';
 
 export interface GetAggregatedParams extends PagedParams {
   orderStatus?: OrderStatus[];
+  devValidationStatus?: boolean | null;
   validationStatus?: boolean | null;
   diabeticOnly?: boolean;
+  submittedOnly?: boolean;
   authorizationStartDate?: string;
   authorizationEndDate?: string;
   searchQuery?: string;
