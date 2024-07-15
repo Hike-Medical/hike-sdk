d442d71ecd9534365c982658566a755b798f36f9
nvarsh-hm <nik@hikemedical.com>
2024-07-15 11:11:33 -0400

    chore: update augment media

--- services/src/hooks/augments/useUpdateAugmentMedia.ts
+++ services/src/hooks/augments/useUpdateAugmentMedia.ts
@@ -2,14 +2,18 @@ import { AugmentMedia, UpdateAugmentMedia } from '@hike/types';
 import { MutationOptions, useMutation } from '@tanstack/react-query';
 import { updateAugmentMedia } from '../../api/augment.service';
 
+interface UpdateAugmentMediaDto {
+  mediaId: string;
+  body: UpdateAugmentMedia;
+}
+
 export const useUpdateAugmentMedia = (
-  mediaId: string,
-  mutationOptions?: MutationOptions<AugmentMedia, Error, UpdateAugmentMedia, unknown>
+  mutationOptions?: MutationOptions<AugmentMedia, Error, UpdateAugmentMediaDto, unknown>
 ) => {
   return useMutation({
-    mutationKey: ['augment', 'media', mediaId],
-    mutationFn: async (updateAugmentMediaDto: UpdateAugmentMedia) => {
-      return await updateAugmentMedia(mediaId, updateAugmentMediaDto);
+    mutationKey: ['updateAugmentMedia'],
+    mutationFn: async ({ mediaId, body }: UpdateAugmentMediaDto) => {
+      return await updateAugmentMedia(mediaId, body);
     },
     ...mutationOptions
   });
