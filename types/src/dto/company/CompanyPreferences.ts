124fdf09bbaba247f1fee6ab2637de63834175bb
Basem Hike <basem@hikemedical.com>
2024-08-01 15:16:53 -0700

    Add more email preferences

--- types/src/dto/company/CompanyPreferences.ts
+++ types/src/dto/company/CompanyPreferences.ts
@@ -1,8 +1,11 @@
 export interface CompanyPreferences {
   carrierServiceCode?: string;
   carrierPreferenceId?: string;
+  distributionEmailAddress?: string;
   preferredSubmittedOrderEmailAddress?: string;
   preferredVeteranEmailAddress?: string;
+  simplrHospitalDeliveryReceiptEmailAddress?: string;
+  simplrWalkInDeliveryReceiptEmailAddress?: string;
   preSubmissionAuth?: boolean;
   requiredSnapshotReview?: boolean;
   modifyTaikaHeelStyle?: boolean;
