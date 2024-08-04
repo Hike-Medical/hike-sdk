426f01fa53d563d8cc1d7c5735e97418c531e3fa
Amrith Balachander <amrith@hikemedical.com>
2024-08-04 12:06:15 -0500

    requested changes

--- types/src/forms/FormField.ts
+++ types/src/forms/FormField.ts
@@ -15,7 +15,7 @@ interface BaseFormField<T extends FormFieldValue> {
   dbField?: { table: string; column: string; unique?: string[] };
   rule?: FormRule;
   templateable?: boolean;
-  remakeable?: boolean;
+  meta?: Record<string, FormFieldValue>;
 }
 
 export type FormField =
