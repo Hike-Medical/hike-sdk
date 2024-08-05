64457d3bc3d5dcbc12443ed416b27170afcd144c
Amrith Balachander <amrith@hikemedical.com>
2024-08-05 18:16:50 -0500

    add streamToString to helper

---
+++ utils/src/helpers/streamToString.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'stream';
+
+export const streamToString = async (stream: Readable): Promise<Buffer> => {
+  return await new Promise((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk, 'base64')));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+};
