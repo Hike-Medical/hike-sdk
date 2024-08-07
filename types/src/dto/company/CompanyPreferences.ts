9dd155fc62331c3f85edc0cf6913d70cab027e3f
Amrith Balachander <amrith@hikemedical.com>
2024-08-07 11:03:53 -0500

    update emailToUser preference

--- types/src/dto/company/CompanyPreferences.ts
+++ types/src/dto/company/CompanyPreferences.ts
@@ -1,7 +1,7 @@
 export interface CompanyPreferences {
   carrierServiceCode?: string;
   carrierPreferenceId?: string;
-  emailToUserAndPreferred?: boolean;
+  emailToUser?: boolean;
   distributionEmailAddress?: string;
   preferredSubmittedOrderEmailAddress?: string;
   preferredVeteranEmailAddress?: string;
