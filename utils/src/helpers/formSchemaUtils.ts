926b6ac0be4224a979ea0273230c32ec0ca7229c
omar abudeh <omar.a@hikemedical.com>
2024-08-30 05:23:08 -0400

    implemented feedback

--- utils/src/helpers/formSchemaUtils.ts
+++ utils/src/helpers/formSchemaUtils.ts
@@ -149,13 +149,12 @@ export const getInvalidSections = (
   sections: FormSection[],
   state: Record<string, FormFieldValue>,
   activeFoot?: string
-): FormSection[] => {
-  return sections.filter((section) =>
+): FormSection[] =>
+  sections.filter((section) =>
     section.fields
       .filter((field) => isFormFieldDisplayed(field, state), activeFoot)
       .some((field, _, fields) => !isFieldValid(field, state, fields.length === 1, activeFoot))
   );
-};
 
 export const schemaStats = (
   sections: FormSection[],
