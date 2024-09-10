25fb044a1f0e8517158a2ddd7063432050e2a80a
omar abudeh <omar.a@hikemedical.com>
2024-09-09 22:01:09 -0400

    added use validator

---
+++ types/src/forms/InvalidValidationInfo.ts
@@ -0,0 +1,12 @@
+export interface InvalidFieldInfo {
+  fieldName: string;
+  fieldLabel: string;
+  sectionIndex: number;
+  sectionTitle: string;
+}
+
+export interface FormValidationResult {
+  isValid: boolean;
+  invalidFieldsData: InvalidFieldInfo[];
+  invalidSectionIndices: number[];
+}
