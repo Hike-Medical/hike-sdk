f84c6e997cfded3d7006caf3fdf5ff8f32da9c11
Basem Hike <basem@hikemedical.com>
2024-09-17 15:27:48 -0400

    Fix company provider for optional context

--- ui/src/providers/CompanyProviderClient.tsx
+++ ui/src/providers/CompanyProviderClient.tsx
@@ -5,14 +5,14 @@ import type { Company } from '@hike/types';
 import { ReactNode, createContext, useContext } from 'react';
 
 interface CompanyProviderClientProps {
-  company: Pick<Company, 'id' | 'name' | 'slug' | 'logoUrl'>;
+  company: Pick<Company, 'id' | 'name' | 'slug' | 'logoUrl'> | null;
   children: ReactNode;
 }
 
-const CompanyContext = createContext<CompanyProviderClientProps['company']>(undefined as never);
+const CompanyContext = createContext<CompanyProviderClientProps['company']>(null);
 
 export const CompanyProviderClient = ({ company, children }: CompanyProviderClientProps) => {
-  configureServices({ companyId: company.id }); // Client-side initialization
+  configureServices({ companyId: company?.id }); // Client-side initialization
   return <CompanyContext.Provider value={company}>{children}</CompanyContext.Provider>;
 };
 
