7e2c4b42e356ff5f26c954a8ca1c4d517d77ee0e
Omar Abudeh <omar.a@hikemedical.com>
2024-09-25 17:41:29 -0400

    Update CreateEvaluationParams.ts

--- types/src/dto/evaluation/CreateEvaluationParams.ts
+++ types/src/dto/evaluation/CreateEvaluationParams.ts
@@ -1,9 +1,6 @@
 import { CareType, Side, VerticalPosition } from '../../../prisma';
-<<<<<<< Updated upstream
 import { CareOrderType } from './../order/CareOrderType';
-=======
-import { CareOrderType } from './../../../../../../../cpo-mono/packages/hike-sdk/types/src/dto/order/CareOrderType';
->>>>>>> Stashed changes
+
 
 /**
  * Data transfer object for creating an evaluation.
@@ -12,11 +9,7 @@ export interface CreateEvaluationParams {
   externalId?: string;
   poNumber?: string;
   type: CareType;
-<<<<<<< Updated upstream
   careOrderType?: CareOrderType;
-=======
-  careOrderType: CareOrderType;
->>>>>>> Stashed changes
   patientId: string;
   deviceTypeId?: string;
   isDiabetic?: boolean;
