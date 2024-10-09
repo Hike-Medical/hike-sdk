eca731a11239d72f6d62e55b6701e3d59b70b65e
Basem Hike <basem@hikemedical.com>
2024-10-09 22:27:35 +0800

    Add constants and error codes

--- utils/src/constants.ts
+++ utils/src/constants.ts
@@ -3,6 +3,8 @@ import type { PaginationState } from '@hike/types';
 export const Constants = {
   AUTH_COOKIE_NAME: 'hike.session-token',
   MIN_PIN_LENGTH: 8,
+  COMPANY_INVITATION_EXPIRATION: 7 * 24 * 60 * 60, // 7 days
+  COMPANY_INVITATION_MAX_LIMIT: 50, // Restricted by Resend service
   DEFAULT_QUERY_LIMIT: 1000,
   DEFAULT_PAGINATION: {
     pageIndex: 0,
