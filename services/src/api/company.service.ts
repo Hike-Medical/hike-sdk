748e66f370f7a2b579c76096bd0301e2dfb7a865
Basem Hike <basem@hikemedical.com>
2024-10-10 23:56:58 +0800

    Add company invite services

--- services/src/api/company.service.ts
+++ services/src/api/company.service.ts
@@ -1,10 +1,19 @@
-import { AddCompanyParams, CompanyExtended } from '@hike/types';
+import {
+  AddCompanyParams,
+  CompanyExtended,
+  CompanyInvitation,
+  CreateCompanyInvitationsParams,
+  DeleteCompanyInvitationsParams,
+  FindCompanyInvitationsParams,
+  PagedResponse,
+  UpdateCompanyInvitationsParams
+} from '@hike/types';
 import { toHikeError } from '../errors/HikeError';
 import { backendApi } from '../utils/backendApi';
 
 export const findCompanyPreferences = async (): Promise<CompanyExtended['preferences']> => {
   try {
-    const response = await backendApi.get(`company/preferences`);
+    const response = await backendApi.get('company/preferences');
     return response.data;
   } catch (error) {
     throw toHikeError(error);
@@ -13,7 +22,7 @@ export const findCompanyPreferences = async (): Promise<CompanyExtended['prefere
 
 export const isFreeTrial = async (): Promise<boolean> => {
   try {
-    const response = await backendApi.get(`auth/isFreeTrial`);
+    const response = await backendApi.get('auth/isFreeTrial');
     return response.data;
   } catch (error) {
     throw toHikeError(error);
@@ -22,7 +31,49 @@ export const isFreeTrial = async (): Promise<boolean> => {
 
 export const addCompany = async (params: AddCompanyParams): Promise<CompanyExtended> => {
   try {
-    const response = await backendApi.post(`company`, params);
+    const response = await backendApi.post('company', params);
+    return response.data;
+  } catch (error) {
+    throw toHikeError(error);
+  }
+};
+
+export const findInvitations = async (
+  params: FindCompanyInvitationsParams
+): Promise<PagedResponse<Omit<CompanyInvitation, 'token'>[]>> => {
+  try {
+    const response = await backendApi.get('company/invite', { params });
+    return response.data;
+  } catch (error) {
+    throw toHikeError(error);
+  }
+};
+
+export const createInvitations = async (
+  params: CreateCompanyInvitationsParams
+): Promise<Omit<CompanyInvitation, 'token'>[]> => {
+  try {
+    const response = await backendApi.post('company/invite', params);
+    return response.data;
+  } catch (error) {
+    throw toHikeError(error);
+  }
+};
+
+export const updateInvitations = async (
+  params: UpdateCompanyInvitationsParams
+): Promise<Omit<CompanyInvitation, 'token'>[]> => {
+  try {
+    const response = await backendApi.patch('company/invite', params);
+    return response.data;
+  } catch (error) {
+    throw toHikeError(error);
+  }
+};
+
+export const revokeInvitations = async (params: DeleteCompanyInvitationsParams): Promise<{ count: number }> => {
+  try {
+    const response = await backendApi.patch(`company/invite/revoke`, params);
     return response.data;
   } catch (error) {
     throw toHikeError(error);
