30a28c170bad1d4848ea982d01ac6d0106b9a518
Amrith Balachander <amrith@hikemedical.com>
2024-10-22 15:46:11 -0500

    add roster to patients

---
+++ types/src/dto/roster/ProcessedRecord.ts
@@ -0,0 +1,15 @@
+export interface ProcessedRecord {
+  firstName: string;
+  lastName: string;
+  employeeId: string;
+  emailAddress?: string | null;
+  phoneNumber?: string | null;
+  jobFunction?: string | null;
+  facilityAddress1?: string | null;
+  facilityAddress2?: string | null;
+  facilityCity?: string | null;
+  facilityState?: string | null;
+  facilityZip?: string | null;
+  department?: string | null;
+  site?: string | null;
+}
