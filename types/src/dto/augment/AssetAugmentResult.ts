7c0f4c9e5301583b7eb3473c2b2a06674aec90bc
Basem Hike <basem@hikemedical.com>
2024-10-22 12:32:00 +0800

    Build fix

--- types/src/dto/augment/AssetAugmentResult.ts
+++ types/src/dto/augment/AssetAugmentResult.ts
@@ -1,4 +1,4 @@
-import { AssetAugmentMedia } from '../../../prisma';
+import { AssetAugment, AssetAugmentMedia } from '../../../prisma';
 import { AssetAugmentMediaData } from './AssetAugmentMediaData';
 
 export interface AssetAugmentResult extends AssetAugment {
