d821243c483cf136accfb6b1b2deec29319d7641
Amrith Balachander <amrith@hikemedical.com>
2024-10-24 18:05:43 -0500

    add extra fields

--- types/src/dto/roster/ProcessedRecord.ts
+++ types/src/dto/roster/ProcessedRecord.ts
@@ -1,5 +1,6 @@
 export interface ProcessedRecord {
   firstName: string;
+  middleName?: string | null;
   lastName: string;
   employeeId: string;
   emailAddress?: string | null;
@@ -11,6 +12,11 @@ export interface ProcessedRecord {
   facilityCity?: string | null;
   facilityState?: string | null;
   facilityZip?: string | null;
+  personalAddress1?: string | null;
+  personalAddress2?: string | null;
+  personalCity?: string | null;
+  personalState?: string | null;
+  personalZip?: string | null;
   department?: string | null;
   site?: string | null;
 }
