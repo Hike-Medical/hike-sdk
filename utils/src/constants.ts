f9ba0ae4510ade5bab4c8b94ad61c7a2f4d2b46f
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2024-10-30 16:27:24 -0500

    update consumer auth (#333)
    * update consumer auth
    
    * Updates and fixes
    
    * Updates and fixes
    
    ---------
    
    Co-authored-by: Basem Hike <basem@hikemedical.com>

--- utils/src/constants.ts
+++ utils/src/constants.ts
@@ -3,7 +3,6 @@ import type { PaginationState } from '@hike/types';
 export const Constants = {
   AUTH_COOKIE_NAME: 'hike.session-token',
   MIN_PIN_LENGTH: 8,
-  SEND_EMAIL_MAX_LIMIT: 50, // Restricted by Resend service
   COMPANY_INVITATION_EXPIRATION: 7 * 24 * 60 * 60 * 1000, // 7 days
   DEFAULT_QUERY_LIMIT: 1000,
   DEFAULT_PAGINATION: {
