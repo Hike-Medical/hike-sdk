0bf22b68179c5b92b1ca64b30e9432b5e905a1fa
Basem Hike <basem@hikemedical.com>
2024-11-29 10:50:44 +0800

    Adjust company invite expiry

--- utils/src/constants.ts
+++ utils/src/constants.ts
@@ -3,7 +3,7 @@ import type { PaginationState } from '@hike/types';
 export const Constants = {
   AUTH_COOKIE_NAME: 'hike.session-token',
   MIN_PIN_LENGTH: 8,
-  COMPANY_INVITATION_EXPIRATION: 30 * 24 * 60 * 60 * 1000, // 30 days
+  COMPANY_INVITATION_EXPIRATION: 14 * 24 * 60 * 60 * 1000, // 14 days
   DEFAULT_QUERY_LIMIT: 1000,
   DEFAULT_PAGINATION: {
     pageIndex: 0,
