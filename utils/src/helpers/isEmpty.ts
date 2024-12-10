32972cffac2c465b7d32ae46d769d0aefa980aa2
Basem Hike <basem@hikemedical.com>
2024-12-10 18:31:49 +0400

    Ensure date is properly tested for is empty

--- utils/src/helpers/isEmpty.ts
+++ utils/src/helpers/isEmpty.ts
@@ -4,4 +4,4 @@ export const isEmpty = (value: unknown) =>
   value == null ||
   (isString(value) && value.trim() === '') ||
   (Array.isArray(value) && value.length === 0) ||
-  (typeof value === 'object' && Object.keys(value).length === 0);
+  (typeof value === 'object' && !(value instanceof Date) && Object.keys(value).length === 0);
