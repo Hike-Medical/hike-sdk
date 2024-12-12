ad6bbd2d8b7c5858283a2bdbf27ad37ee98c644b
nvarsh-hm <nik@hikemedical.com>
2024-12-12 14:33:43 -0600

    chore: add typings

--- types/src/dto/notify/SlackMessage.ts
+++ types/src/dto/notify/SlackMessage.ts
@@ -1,10 +1,24 @@
 export interface SlackMessage {
   channelUrl: string | undefined;
   text: string;
-  attachments?: {
-    text?: string;
+  attachments?: Array<{
+    mrkdwn_in: Array<string>;
     color: string;
-    title?: string;
-    fields?: { title?: string; value?: string; short?: boolean }[];
-  }[];
+    pretext: string;
+    author_name: string;
+    author_link: string;
+    author_icon: string;
+    title: string;
+    title_link: string;
+    text: string;
+    fields: Array<{
+      title: string;
+      value: string;
+      short: boolean;
+    }>;
+    thumb_url: string;
+    footer: string;
+    footer_icon: string;
+    ts: number;
+  }>;
 }
