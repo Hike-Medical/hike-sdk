662c33596b11680ac17feddf26497e2259fcd011
nvarsh-hm <nik@hikemedical.com>
2024-12-12 14:41:10 -0600

    fix: mark things optional

--- types/src/dto/notify/SlackMessage.ts
+++ types/src/dto/notify/SlackMessage.ts
@@ -1,24 +1,25 @@
 export interface SlackMessage {
   channelUrl: string | undefined;
-  text: string;
+  text?: string;
+  pretext?: string;
   attachments?: Array<{
-    mrkdwn_in: Array<string>;
-    color: string;
-    pretext: string;
-    author_name: string;
-    author_link: string;
-    author_icon: string;
-    title: string;
-    title_link: string;
-    text: string;
-    fields: Array<{
-      title: string;
-      value: string;
-      short: boolean;
+    mrkdwn_in?: Array<string>;
+    color?: string;
+    pretext?: string;
+    author_name?: string;
+    author_link?: string;
+    author_icon?: string;
+    title?: string;
+    title_link?: string;
+    text?: string;
+    fields?: Array<{
+      title?: string;
+      value?: string;
+      short?: boolean;
     }>;
-    thumb_url: string;
-    footer: string;
-    footer_icon: string;
-    ts: number;
+    thumb_url?: string;
+    footer?: string;
+    footer_icon?: string;
+    ts?: number;
   }>;
 }
