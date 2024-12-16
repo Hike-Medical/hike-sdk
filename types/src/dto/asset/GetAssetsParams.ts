2f2759b63f38241f8a57775fe99111fe58056ef8
Basem Hike <basem@hikemedical.com>
2024-12-16 13:16:21 -0600

    Extend assets param

--- types/src/dto/asset/GetAssetsParams.ts
+++ types/src/dto/asset/GetAssetsParams.ts
@@ -2,6 +2,15 @@ import { Prisma } from '../../../prisma';
 import type { PagedParams } from '../PagedParams';
 
 export interface GetAssetsParams extends PagedParams {
-  sortBy?: 'createdAt' | 'updatedAt' | 'detectionUpdatedAt';
+  filter?: Record<'type' | 'status' | 'foot.patientId' | 'foot.side' | 'foot.workbench.status', string>;
+  sortBy?:
+    | 'type'
+    | 'status'
+    | 'active'
+    | 'createdAt'
+    | 'updatedAt'
+    | 'detectionUpdatedAt'
+    | 'foot.side'
+    | 'foot.workbench.status';
   sortOrder?: Prisma.SortOrder;
 }
