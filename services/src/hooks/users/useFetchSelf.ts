6bfd92862af533decd3547bcdc7766ecc822f337
Amrith Balachander <amrith@hikemedical.com>
2024-12-24 11:49:12 -0600

    update self

--- services/src/hooks/users/useFetchSelf.ts
+++ services/src/hooks/users/useFetchSelf.ts
@@ -3,12 +3,12 @@ import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
 import { fetchSelf } from '../../api/user.service';
 import { HikeError } from '../../errors/HikeError';
 
-export interface UseUsersOptions
+export interface useFetchSelfOptions
   extends Omit<UseQueryOptions<SafeUserExtended, HikeError<null>>, 'queryKey' | 'queryFn'> {
   queryKey?: QueryKey;
 }
 
-export const useUsers = ({ queryKey = [], ...options }: UseUsersOptions) =>
+export const useFetchSelf = ({ queryKey = [], ...options }: useFetchSelfOptions) =>
   useQuery({
     queryKey: ['selfUser', queryKey],
     queryFn: async () => await fetchSelf(),
