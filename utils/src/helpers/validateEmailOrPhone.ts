aef42df8eb2d387cc61d86e2029a893d8391220d
Basem Hike <basem@hikemedical.com>
2024-12-30 13:01:29 +0800

    Add email and phone validators

--- utils/src/helpers/validateEmailOrPhone.ts
+++ utils/src/helpers/validateEmailOrPhone.ts
@@ -1,15 +1,14 @@
 /**
  * Validates if the input is a valid email address or phone number.
  */
-export const validateEmailOrPhone = (input: string): boolean => {
-  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input)) {
-    return true;
-  }
+export const validateEmailOrPhone = (input: string): boolean => validateEmail(input) || validatePhone(input);
 
-  // Allows spaces, parentheses, and dashes
-  if (/^[\s()+-]*([0-9][\s()+-]*){10,15}$/.test(input)) {
-    return true;
-  }
+/**
+ * Validates if the input is a valid email address.
+ */
+export const validateEmail = (input: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
 
-  return false;
-};
+/**
+ * Validates if the input is a valid phone number. Allows spaces, parentheses, and dashes.
+ */
+export const validatePhone = (input: string): boolean => /^[\s()+-]*([0-9][\s()+-]*){10,15}$/.test(input);
