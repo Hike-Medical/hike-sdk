c36ca14455998b8502947dd6611230d95cd12796
nvarsh-hm <154340007+nvarsh-hm@users.noreply.github.com>
2025-01-16 20:32:32 -0500

    chore: add company ids to header (#456)

--- services/src/hooks/workbenches/useWorkbenchStats.ts
+++ services/src/hooks/workbenches/useWorkbenchStats.ts
@@ -5,12 +5,13 @@ import { HikeError } from '../../errors/HikeError';
 
 export interface UseWorkbenchStatsOptions
   extends Omit<UseQueryOptions<{ status: WorkbenchStatus; count: number }[], HikeError<null>>, 'queryKey' | 'queryFn'> {
+  companyIds?: string[];
   queryKey?: QueryKey;
 }
 
-export const useWorkbenchStats = ({ queryKey = [], ...options }: UseWorkbenchStatsOptions = {}) =>
+export const useWorkbenchStats = ({ companyIds = [], queryKey = [], ...options }: UseWorkbenchStatsOptions = {}) =>
   useQuery({
     queryKey: ['workbenchStats', queryKey],
-    queryFn: async () => await statsForWorkbenches(),
+    queryFn: async () => await statsForWorkbenches(companyIds),
     ...options
   });
