c1caca4692b33dacb05dd1cc55b60a558d0229ca
Basem Hike <basem@hikemedical.com>
2025-01-23 18:05:59 +0800

    Add roster deactivation types

---
+++ services/src/hooks/roster/useRosterDeactivateJob.ts
@@ -0,0 +1,20 @@
+import type { DeactivateRosterResponse } from '@hike/types';
+import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { fetchRosterDeactivateStatus } from '../../api/roster.service';
+import { HikeError } from '../../errors/HikeError';
+
+export interface UseRosterDeactivateJobOptions
+  extends Omit<
+    UseQueryOptions<{ progress: number; data: DeactivateRosterResponse }, HikeError<null>>,
+    'queryKey' | 'queryFn'
+  > {
+  id: string;
+  queryKey?: QueryKey;
+}
+
+export const useRosterDeactivateJob = ({ id, queryKey = [], ...options }: UseRosterDeactivateJobOptions) =>
+  useQuery({
+    queryKey: ['rosterDeactivateJob', id, queryKey],
+    queryFn: async () => await fetchRosterDeactivateStatus(id),
+    ...options
+  });
