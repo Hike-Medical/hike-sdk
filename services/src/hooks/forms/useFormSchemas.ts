531f7b8d8700e5e34051c0c04ce393e9f0762516
Basem <basem@hikemedical.com>
2025-03-19 07:10:11 +0800

    Extend form API (#526)

--- services/src/hooks/forms/useFormSchemas.ts
+++ services/src/hooks/forms/useFormSchemas.ts
@@ -1,17 +1,17 @@
 import type { FormSchemaTyped } from '@hike/types';
 import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
-import { findFormSchemasByIds } from '../../api/form.service';
+import { findFormSchemas } from '../../api/form.service';
 import { HikeError } from '../../errors/HikeError';
 
 interface UseFormSchemasOptions
   extends Omit<UseQueryOptions<FormSchemaTyped[] | null, HikeError<null>>, 'queryKey' | 'queryFn'> {
-  schemaIds: string[];
+  schemaIds?: string[];
   queryKey?: QueryKey;
 }
 
-export const useFormSchemas = ({ schemaIds, queryKey = [], ...options }: UseFormSchemasOptions) =>
+export const useFormSchemas = ({ schemaIds, queryKey = [], ...options }: UseFormSchemasOptions = {}) =>
   useQuery({
     queryKey: ['formSchemas', schemaIds, queryKey],
-    queryFn: async () => await findFormSchemasByIds(schemaIds),
+    queryFn: async () => await findFormSchemas(schemaIds),
     ...options
   });
