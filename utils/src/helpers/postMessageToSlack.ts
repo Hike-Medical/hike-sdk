2e6db9f32f6a32fe9c1f7f265d69228870c53796
Basem Hike <basem@hikemedical.com>
2025-03-27 14:54:18 +0800

    Consolidate Slack parameter

--- utils/src/helpers/postMessageToSlack.ts
+++ utils/src/helpers/postMessageToSlack.ts
@@ -1,4 +1,28 @@
-import { SlackMessage } from '@hike/types';
+export interface SlackMessage {
+  channelUrl: string | undefined;
+  text?: string;
+  pretext?: string;
+  attachments?: Array<{
+    mrkdwn_in?: Array<string>;
+    color?: string;
+    pretext?: string;
+    author_name?: string;
+    author_link?: string;
+    author_icon?: string;
+    title?: string;
+    title_link?: string;
+    text?: string;
+    fields?: Array<{
+      title?: string;
+      value?: string;
+      short?: boolean;
+    }>;
+    thumb_url?: string;
+    footer?: string;
+    footer_icon?: string;
+    ts?: number;
+  }>;
+}
 
 export const postMessageToSlack = async ({ channelUrl, text, attachments }: SlackMessage) => {
   if (!channelUrl) {
