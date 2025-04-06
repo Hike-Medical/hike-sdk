61126b6441c108f7d231edbb9848e336c4582187
nvarsh-hm <154340007+nvarsh-hm@users.noreply.github.com>
2025-04-06 06:12:29 +0530

    feat: add ovewrite signature to params (#553)

--- types/src/dto/workbench/SubmitOrderParams.ts
+++ types/src/dto/workbench/SubmitOrderParams.ts
@@ -2,5 +2,6 @@ import { OrderAuthorizationStatus } from '../../../prisma';
 
 export interface SubmitOrderParams {
   signatureBase64Data: string;
+  overwriteSignature?: boolean;
   orderAuthorizationStatus?: OrderAuthorizationStatus;
 }
