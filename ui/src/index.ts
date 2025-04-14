b03121b3426b15dcbfee3b9c302d28ead20f18f4
Basem Hike <basem@hikemedical.com>
2025-04-14 16:27:45 +0200

    Update async company layout

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,4 +1,4 @@
-export * from './providers/CompanyProvider';
+export { default as CompanyProvider } from './providers/CompanyProvider';
 export { useCompany } from './providers/CompanyProviderClient';
 export * from './providers/HikeProvider';
 export * from './providers/SessionProvider';
