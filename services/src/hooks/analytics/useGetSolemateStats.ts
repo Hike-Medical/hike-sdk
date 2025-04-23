cd03df279ed06a130ac5e8047be3babda39ad8ff
Amrith Balachander <amrith@hikemedical.com>
2025-04-23 17:35:45 -0400

    update solemate stats

--- services/src/hooks/analytics/useGetSolemateStats.ts
+++ services/src/hooks/analytics/useGetSolemateStats.ts
@@ -1,20 +1,10 @@
+import { SolemateStatus } from '@hike/utils';
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { getSolemateStats } from '../../api/analytics.service';
 import { HikeError } from '../../errors/HikeError';
 
 interface UseGetSolemateStatsOptions
-  extends Omit<
-    UseQueryOptions<
-      {
-        devOrFailed: number;
-        needsValidation: number;
-        needsManufacturing: number;
-        inProduction: number;
-      },
-      HikeError<null>
-    >,
-    'queryKey' | 'queryFn'
-  > {}
+  extends Omit<UseQueryOptions<Record<SolemateStatus, number>, HikeError<null>>, 'queryKey' | 'queryFn'> {}
 
 export const useGetSolemateStats = (companyIds: string[], queryOptions?: UseGetSolemateStatsOptions) =>
   useQuery({
