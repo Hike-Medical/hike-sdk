a8c539010b3ddfa70ccc8654b3475ddff419b9d6
Basem Hike <basem@hikemedical.com>
2025-04-26 05:21:42 +0800

    Minot updates

--- ui/src/providers/CompanyProviderClient.tsx
+++ ui/src/providers/CompanyProviderClient.tsx
@@ -4,15 +4,15 @@ import { configureServices } from '@hike/services';
 import type { HikeConfig, SafeCompany } from '@hike/types';
 import { ReactNode, createContext } from 'react';
 
-interface CompanyState {
+export const CompanyContext = createContext<SafeCompany | null>(null);
+
+interface CompanyProviderClientProps {
   company: SafeCompany | null;
   config: HikeConfig;
   children: ReactNode;
 }
 
-export const CompanyContext = createContext<CompanyState['company']>(null);
-
-export const CompanyProviderClient = ({ company, config, children }: CompanyState) => {
+export const CompanyProviderClient = ({ company, config, children }: CompanyProviderClientProps) => {
   configureServices({ ...config, companyId: company?.id }); // Client-side initialization
   return <CompanyContext value={company}>{children}</CompanyContext>;
 };
