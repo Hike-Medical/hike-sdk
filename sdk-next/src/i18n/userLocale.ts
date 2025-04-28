4194cae284afa21d09528933d41a6af697c61694
Basem Hike <basem@hikemedical.com>
2025-04-28 23:37:22 +0800

    Move maintenance logic to base middleware

--- sdk-next/src/i18n/userLocale.ts
+++ sdk-next/src/i18n/userLocale.ts
@@ -3,6 +3,9 @@
 import { Constants } from '@hike/sdk';
 import { cookies } from 'next/headers';
 
+/**
+ * Get the user's locale from the cookie.
+ */
 export const getUserLocale = async (): Promise<{ locale: string; slug?: string }> => {
   const cookieStore = await cookies();
   const cookieValue = cookieStore.get(Constants.i18n.LOCALE_COOKIE_NAME)?.value;
@@ -10,6 +13,9 @@ export const getUserLocale = async (): Promise<{ locale: string; slug?: string }
   return { locale: locale || Constants.i18n.DEFAULT_LOCALE, slug };
 };
 
+/**
+ * Set the user's locale in the cookie.
+ */
 export const setUserLocale = async (locale: string, slug?: string) => {
   const cookieStore = await cookies();
   cookieStore.set(Constants.i18n.LOCALE_COOKIE_NAME, `${locale}${slug ? `.${slug}` : ''}`);
