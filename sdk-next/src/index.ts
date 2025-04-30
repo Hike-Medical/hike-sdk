3eb3c909029d6bf3f6d98f0433ea91535b298968
Basem <basem@hikemedical.com>
2025-05-01 03:18:46 +0800

    Move i18n dynamic path to app (#606)
    * i18n dynamic path
    
    * Move to app

--- sdk-next/src/index.ts
+++ sdk-next/src/index.ts
@@ -6,7 +6,6 @@ export * from './contexts/PostHogProvider';
 export * from './contexts/PylonProvider';
 export * from './hooks/useIsMobile';
 export * from './hooks/useMobilePlatform';
-export * from './i18n/getLocaleMessages';
 export * from './i18n/userLocale';
 export * from './media/camera';
 export * from './media/getBestSupportedVideoFormat';
