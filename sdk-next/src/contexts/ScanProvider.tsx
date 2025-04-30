680f8cff9637981492972784e6de41386fc5afee
nvarsh-hm <nik@hikemedical.com>
2025-04-30 09:18:09 -0400

    fix: add optimal video constraints

--- sdk-next/src/contexts/ScanProvider.tsx
+++ sdk-next/src/contexts/ScanProvider.tsx
@@ -5,3 +5,11 @@ export enum CameraPermissionState {
   DENIED = 'denied', // Permission denied
   INITIALIZING = 'initializing' // Permission granted, camera initializing
 }
+
+export const OPTIMAL_VIDEO_CONSTRAINTS = {
+  audio: false,
+  video: {
+    facingMode: { ideal: 'user' }, // Will be updated based on mode
+    aspectRatio: { ideal: 16 / 9 } // Consistent aspect ratio
+  }
+};
