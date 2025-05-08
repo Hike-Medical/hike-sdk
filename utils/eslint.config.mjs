73a0c1e76ddd54cf48e8addac99e94f080eb70d1
Basem Hike <basem@hikemedical.com>
2025-05-08 21:33:09 +0800

    Update TS config for config files

--- utils/eslint.config.mjs
+++ utils/eslint.config.mjs
@@ -1,4 +1,1 @@
-import { config } from '@hike/eslint-config/base';
-
-/** @type {import("eslint").Linter.Config} */
-export default config;
+export { config as default } from '@hike/eslint-config/base';
