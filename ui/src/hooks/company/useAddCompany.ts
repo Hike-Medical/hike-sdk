d01d943914bf375f4bc6615b0b272cc845ea8e33
Basem <basem@hikemedical.com>
2025-05-10 05:00:14 +0800

    Create and assign clinician services (#621)

--- ui/src/hooks/company/useAddCompany.ts
+++ ui/src/hooks/company/useAddCompany.ts
@@ -2,11 +2,11 @@ import { HikeError, addCompany } from '@hike/services';
 import { AddCompanyParams, CompanyExtended } from '@hike/types';
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
 
-interface AddCompanyContext {
+interface AddCompanyOptions {
   body: AddCompanyParams;
 }
 
-export const useAddCompany = (options?: UseMutationOptions<CompanyExtended, HikeError<null>, AddCompanyContext>) =>
+export const useAddCompany = (options?: UseMutationOptions<CompanyExtended, HikeError<null>, AddCompanyOptions>) =>
   useMutation({
     mutationKey: ['addCompany'],
     mutationFn: async ({ body }) => await addCompany(body),
