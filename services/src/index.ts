0668b6baa565158f7520152fa2ad1443ba5d32ac
Basem Hike <basem@hikemedical.com>
2025-05-09 18:04:31 +0800

    Extract clinician services

--- services/src/index.ts
+++ services/src/index.ts
@@ -6,6 +6,7 @@ export * from './api/auth.service';
 export * from './api/auto-slicer.service';
 export * from './api/billing-code.service';
 export * from './api/catalog.service';
+export * from './api/clinician.service';
 export * from './api/company.service';
 export * from './api/device-type.service';
 export * from './api/diagnosis.service';
