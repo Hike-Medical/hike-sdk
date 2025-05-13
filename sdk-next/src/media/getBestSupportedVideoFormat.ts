b4046ffa596373815f499f739ff0e59e0c015ba1
nvarsh-hm <nik@hikemedical.com>
2025-05-13 11:00:18 -0400

    fix: force webm on android and mp4 on ios

--- sdk-next/src/media/getBestSupportedVideoFormat.ts
+++ sdk-next/src/media/getBestSupportedVideoFormat.ts
@@ -1,35 +1,22 @@
+import { isIOS } from '@hike/sdk';
 import { getSupportedMimeTypes } from './getSupportedMimeTypes';
 
 /**
  * Determines the best supported video format based on a predefined list of video types and codecs.
- * The function returns the first supported format found. If no supported format is found, it defaults to 'video/mp4'.
+ * The function returns the first supported format found.
  *
- * @returns The best supported video format or 'video/mp4' if none of the formats are supported.
- */
+ * Prefer MP4 on iOS, WebM elsewhere.
+ * @returns The best supported video format or fallsback to a safe default if the preferred list is empty.
+ * */
 export const getBestSupportedVideoFormat = (): string => {
-  const containers = ['mp4', 'webm', 'ogg', 'x-matroska'];
-  const preferredCodecs = [
-    'avc1.42E01E',
-    'avc1',
-    'vp9',
-    'vp9.0',
-    'vp8',
-    'vp8.0',
-    'avc1',
-    'av1',
-    'h265',
-    'h.265',
-    'h264',
-    'h.264',
-    'opus',
-    'pcm',
-    'aac',
-    'mpeg',
-    'mp4a'
-  ];
+  const ios = isIOS();
+  const containers = ios ? ['mp4'] : ['webm', 'ogg', 'x-matroska'];
+  const preferredCodecs = ios
+    ? ['avc1.42E01E', 'h264', 'mp4a.40.2', 'aac'] // H.264 + AAC
+    : ['vp9', 'vp8', 'avc1', 'h264', 'opus']; // VP9/8 first
 
   const supportedVideos = getSupportedMimeTypes('video', containers, preferredCodecs, containers);
 
-  // Default to webm if no preferred mp4 format is not possible
-  return supportedVideos[0] || 'video/webm;codecs=vp8';
+  // OS based fallback based off AI preferences
+  return supportedVideos[0] || (ios ? 'video/mp4' : 'video/webm;codecs="vp8"');
 };
