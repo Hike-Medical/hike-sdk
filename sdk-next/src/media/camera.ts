b75042d5e84e8a9495d4add4834f11d81970e807
Basem Hike <basem@hikemedical.com>
2025-05-21 11:21:31 +0800

    Undo formatting

--- sdk-next/src/media/camera.ts
+++ sdk-next/src/media/camera.ts
@@ -5,9 +5,7 @@
  *
  * @returns A promise that resolves with the media stream if permissions are granted.
  */
-export const requestCameraPermissions = (
-  constraints: MediaStreamConstraints = {}
-): Promise<MediaStream> =>
+export const requestCameraPermissions = (constraints: MediaStreamConstraints = {}): Promise<MediaStream> =>
   new Promise((resolve, reject) => {
     navigator.mediaDevices
       .getUserMedia(constraints)
