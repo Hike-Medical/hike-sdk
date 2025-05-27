54a77a7f5885713e1bb06af02098b2b62309b61c
Basem <basem@hikemedical.com>
2025-05-27 11:05:35 +0800

    Add form submission utils (#681)

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -27,6 +27,7 @@ export * from './helpers/extractDomain';
 export * from './helpers/findKeyInNested';
 export * from './helpers/formAddressUtils';
 export * from './helpers/formSchemaUtils';
+export * from './helpers/formSubmissionUtils';
 export * from './helpers/getLocalizedDateFormat';
 export * from './helpers/httpUtils';
 export * from './helpers/isIOS';
