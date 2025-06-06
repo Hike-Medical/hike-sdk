13c4909b87cbb319b38b96e61fa217e19036b218
Basem <basem@hikemedical.com>
2025-06-07 02:25:48 +0800

    Explicitly request a higher resolution feed to avoid blurry video (#712)
    * Explicitly request a higher resolution feed to avoid blurry video
    
    * Update for lower end cameras

--- sdk-next/src/contexts/ScanProvider.tsx
+++ sdk-next/src/contexts/ScanProvider.tsx
@@ -10,6 +10,8 @@ export const OPTIMAL_VIDEO_CONSTRAINTS = {
   audio: false,
   video: {
     facingMode: { ideal: 'user' }, // Will be updated based on mode
-    aspectRatio: { ideal: 16 / 9 } // Consistent aspect ratio
+    aspectRatio: { ideal: 16 / 9 }, // Consistent aspect ratio
+    width: { min: 640, ideal: 1280, max: 1920 },
+    height: { min: 480, ideal: 720, max: 1080 }
   }
-};
+} satisfies MediaStreamConstraints;
