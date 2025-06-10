1cef754c88e113a619f8673173d99688902a998c
Basem <basem@hikemedical.com>
2025-06-11 00:09:01 +0800

    Add evaluation and roster import date format options (#719)

--- types/src/dto/roster/ImportRosterParams.ts
+++ types/src/dto/roster/ImportRosterParams.ts
@@ -3,6 +3,7 @@ import { RoasterPatientCsvRecord } from './RoasterPatientCsvRecord';
 export interface ImportRosterParams {
   fileKey: string;
   columnMapping: Partial<Record<keyof RoasterPatientCsvRecord, string>>;
+  dateFormat?: string;
   tag?: string;
   onlyTagCreated?: boolean;
 }
