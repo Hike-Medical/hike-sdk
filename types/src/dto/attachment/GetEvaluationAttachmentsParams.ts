87905b23411dbc546ada87f1615acd2a5059c5e0
Amrith Balachander <amrith@hikemedical.com>
2025-06-16 10:10:00 -0400

    update sdk commit

--- types/src/dto/attachment/GetEvaluationAttachmentsParams.ts
+++ types/src/dto/attachment/GetEvaluationAttachmentsParams.ts
@@ -1,4 +1,4 @@
-import { EvaluationAttachmentStatus } from '@prisma/client';
+import { EvaluationAttachmentStatus } from '../../../prisma';
 import type { PagedParams } from '../PagedParams';
 
 export interface GetEvaluationAttachmentsParams extends PagedParams {
