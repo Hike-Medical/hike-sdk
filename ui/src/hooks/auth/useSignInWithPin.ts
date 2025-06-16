eb973b2ceab58951f3e6ed0a9f4f66ba2095a165
Basem Hike <basem@hikemedical.com>
2025-06-16 14:06:02 +0800

    Update pin signin API

--- ui/src/hooks/auth/useSignInWithPin.ts
+++ ui/src/hooks/auth/useSignInWithPin.ts
@@ -1,8 +1,10 @@
 import { signInWithPin } from '@hike/services';
-import { AuthSession, HikeError, SignInWithPinBody } from '@hike/types';
+import { HikeError, SignInWithPinBody } from '@hike/types';
 import { UseMutationOptions, useMutation } from '@tanstack/react-query';
 
-export const useSignInWithPin = (options?: UseMutationOptions<AuthSession, HikeError<null>, SignInWithPinBody>) =>
+export const useSignInWithPin = (
+  options?: UseMutationOptions<{ accessToken: string }, HikeError<null>, SignInWithPinBody>
+) =>
   useMutation({
     mutationKey: ['signInWithPin'],
     mutationFn: async (credentials) => await signInWithPin(credentials),
