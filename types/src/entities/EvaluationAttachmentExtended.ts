07f01263a3167094f1fdd6b933e7a4914e81d4e4
nvarsh-hm <nik@hikemedical.com>
2025-06-16 16:32:10 -0400

    fix: remove metadata override

--- types/src/entities/EvaluationAttachmentExtended.ts
+++ types/src/entities/EvaluationAttachmentExtended.ts
@@ -1,7 +1,6 @@
 import { Evaluation, EvaluationAttachment, NotificationHistory, Patient } from '../../prisma';
 
 export type EvaluationAttachmentExtended = EvaluationAttachment & {
-  metadata: Record<string, any>;
   evaluation:
     | (Evaluation & {
         patient: Patient & {
