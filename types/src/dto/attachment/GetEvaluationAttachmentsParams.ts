3ccbf20be1499ebbf83aae8a82a396389ffab396
Amrith Balachander <amrith@hikemedical.com>
2025-06-17 09:50:15 -0400

    filter eval types by array

--- types/src/dto/attachment/GetEvaluationAttachmentsParams.ts
+++ types/src/dto/attachment/GetEvaluationAttachmentsParams.ts
@@ -4,5 +4,5 @@ import type { PagedParams } from '../PagedParams';
 export interface GetEvaluationAttachmentsParams extends PagedParams {
   statuses?: EvaluationAttachmentStatus[];
   evaluationId?: string;
-  type?: EvaluationAttachmentType;
+  types?: EvaluationAttachmentType[];
 }
