80972bb0aff2f69a8549628b23dc2887a608aea7
nvarsh-hm <nik@hikemedical.com>
2025-06-25 08:03:57 -0400

    feat: refactor security header code

--- sdk-next/src/index.ts
+++ sdk-next/src/index.ts
@@ -30,4 +30,3 @@ export * from './themes/types/ThemeComponent';
 export * from './types/TablerIconComponent';
 export * from './utils/analytics';
 export * from './utils/logger';
-export * from './config/withSecurityHeaders';
