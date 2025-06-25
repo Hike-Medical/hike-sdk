61b4d1830ef5e5505f921563781781f446cd9599
nvarsh-hm <nik@hikemedical.com>
2025-06-24 22:29:23 -0400

    fix: try changing paths

--- sdk-next/src/index.ts
+++ sdk-next/src/index.ts
@@ -30,4 +30,4 @@ export * from './themes/types/ThemeComponent';
 export * from './types/TablerIconComponent';
 export * from './utils/analytics';
 export * from './utils/logger';
-export * from './next.config';
+export * from './config/withSecurityHeaders';
