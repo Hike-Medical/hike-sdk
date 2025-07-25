e644f513e1be0c89658112e470d6f256e721ce53
Amrith Balachander <amrith@hikemedical.com>
2025-07-25 15:23:02 -0400

    fix build error

--- ui/src/hooks/search/useSqlSearchQuery.ts
+++ ui/src/hooks/search/useSqlSearchQuery.ts
@@ -1,6 +1,6 @@
+import { sqlSearchQuery } from '@hike/services';
 import { HikeError, SqlQueryParams } from '@hike/types';
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import { sqlSearchQuery } from '../../api/search.service';
 
 interface SqlSearchQueryContext extends Omit<UseQueryOptions<unknown, HikeError<null>>, 'queryFn' | 'queryKey'> {
   params: SqlQueryParams;
