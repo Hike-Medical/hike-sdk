7f3c5d3b5c9148a28d77d34617e998282d2585d0
Basem <basem@hikemedical.com>
2025-08-08 16:17:32 +0800

    Update notification types with optionals (#811)
    * Update notification types with optionals
    
    * Make notification company optional

--- types/src/entities/NotificationHistoryExtended.ts
+++ types/src/entities/NotificationHistoryExtended.ts
@@ -1,5 +1,5 @@
 import { CompanyPatient, NotificationHistory, Patient, User } from '../../prisma';
 
 export type NotificationHistoryExtended = NotificationHistory & {
-  patient: Patient & { companies: (CompanyPatient & { user: User | null })[] };
+  patient: (Patient & { companies: (CompanyPatient & { user: User | null })[] }) | null;
 };
