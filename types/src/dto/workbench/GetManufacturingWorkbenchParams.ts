26fb0fd35d762bf7031cb54c98253a4d943499ab
nvarsh-hm <nik@hikemedical.com>
2025-08-11 11:09:33 -0400

    fix: add prioritize rush orders

--- types/src/dto/workbench/GetManufacturingWorkbenchParams.ts
+++ types/src/dto/workbench/GetManufacturingWorkbenchParams.ts
@@ -1,3 +1,5 @@
 import { GetAggregatedParams } from './GetAggregatedParams';
 
-export interface GetManufacturingWorkbenchParams extends GetAggregatedParams {}
+export interface GetManufacturingWorkbenchParams extends GetAggregatedParams {
+  prioritizeRushOrders?: boolean;
+}
