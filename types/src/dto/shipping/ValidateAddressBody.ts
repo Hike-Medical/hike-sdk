522095c76e76195334d3bfaee1ab80ea2d302a3a
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2025-08-12 10:08:51 -0400

    update sdk for updated service calls (#818)
    * update sdk for updated service calls
    
    * added requested changes

--- types/src/dto/shipping/ValidateAddressBody.ts
+++ types/src/dto/shipping/ValidateAddressBody.ts
@@ -1,9 +1,49 @@
 export interface ValidateAddressBody {
-  name: string;
   addressLine1: string;
   addressLine2?: string;
   city: string;
   stateOrProvince: string;
   postalCode: string;
-  country: string;
+  countryCode?: string;
+}
+
+export interface ShippingAddressBody extends ValidateAddressBody {
+  phone?: string;
+  name: string;
+  isResidential?: boolean;
+}
+
+export interface ShipEngineValidateAddressResponse {
+  status: string;
+  original_address: OriginalAddress;
+  matched_address: MatchedAddress;
+  messages: unknown[];
+}
+
+interface OriginalAddress {
+  name: string;
+  phone: string;
+  company_name: string;
+  address_line1: string;
+  address_line2: string;
+  address_line3: string;
+  city_locality: string;
+  state_province: string;
+  postal_code: number;
+  country_code: string;
+  address_residential_indicator: string;
+}
+
+interface MatchedAddress {
+  name: string;
+  phone: string;
+  company_name: string;
+  address_line1: string;
+  address_line2: string;
+  address_line3: string;
+  city_locality: string;
+  state_province: string;
+  postal_code: string;
+  country_code: string;
+  address_residential_indicator: string;
 }
