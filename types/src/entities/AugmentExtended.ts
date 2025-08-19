a208d84cd522bf125f4c519a652c85133ffecc40
nvarsh-hm <nik@hikemedical.com>
2025-08-19 17:10:41 -0400

    feat: add userId to AssetAugment

--- types/src/entities/AugmentExtended.ts
+++ types/src/entities/AugmentExtended.ts
@@ -1,6 +1,10 @@
 import type { AssetAugment, AssetAugmentMedia, Asset, Foot, Patient, CompanyPatient, Workbench } from '../../prisma';
 
 export type AugmentExtended = AssetAugment & {
+  /**
+   * The user identifier associated with the augment's patient.
+   */
+  userId?: string | null;
   media: AssetAugmentMedia[];
   asset: Asset & {
     foot: Foot & {
