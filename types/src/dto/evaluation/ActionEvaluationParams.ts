d9969d270424db78cdb84f8738fdaa6ebf45aacf
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2025-08-29 10:00:30 -0400

    refactor action evaluatoin (#832)

--- types/src/dto/evaluation/ActionEvaluationParams.ts
+++ types/src/dto/evaluation/ActionEvaluationParams.ts
@@ -1,8 +1,9 @@
+import { WorkbenchCreatedReason } from '../../../prisma';
+
 /**
  * Data transfer object for creating an evaluation as an insole.
  */
 export interface ActionEvaluationParams {
-  evaluationId: string;
+  action: WorkbenchCreatedReason;
   notes?: string;
-  companyIds?: string[];
 }
