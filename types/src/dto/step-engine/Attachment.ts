393156b086af1949688ec8fc97992a9bee168c90
Gabriel Fishman <gabriel.fishman@hikemedical.com>
2025-09-04 21:55:00 -0400

    add attachment types, add created attachment status

--- types/src/dto/step-engine/Attachment.ts
+++ types/src/dto/step-engine/Attachment.ts
@@ -4,7 +4,10 @@ const EvaluationAttachmentTypeEnum = {
   REFERRING_PHYSICIAN: 'REFERRING_PHYSICIAN',
   PRIMARY_PHYSICIAN: 'PRIMARY_PHYSICIAN',
   APPOINTMENT_IMPORT: 'APPOINTMENT_IMPORT',
-  FORM_SUBMISSION: 'FORM_SUBMISSION'
+  FORM_SUBMISSION: 'FORM_SUBMISSION',
+  FAX: 'FAX',
+  OCR_OUTPUT: 'OCR_OUTPUT',
+  EXTRACTION_OUTPUT: 'EXTRACTION_OUTPUT'
 } as const satisfies Record<EvaluationAttachmentType, EvaluationAttachmentType> & {
   [K in EvaluationAttachmentType]: K;
 };
@@ -12,6 +15,7 @@ const EvaluationAttachmentTypeEnum = {
 export const EvaluationAttachmentTypeList = Object.values(EvaluationAttachmentTypeEnum);
 
 const EvaluationAttachmentStatusEnum = {
+  CREATED: 'CREATED',
   PENDING: 'PENDING',
   APPROVED: 'APPROVED',
   REJECTED: 'REJECTED',
