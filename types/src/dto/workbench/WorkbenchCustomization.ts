515c3f7f00a6519c5be997b010cc059317133b97
Amrith Balachander <amrith@hikemedical.com>
2025-09-10 14:03:55 -0400

    remove customization

--- types/src/dto/workbench/WorkbenchCustomization.ts
+++ types/src/dto/workbench/WorkbenchCustomization.ts
@@ -4,5 +4,4 @@ import { ClinicalFlowType } from '../preferences/ClinicalFlowType';
 export interface WorkbenchCustomization {
   flowType?: ClinicalFlowType;
   scanType?: RenderType;
-  externalId?: string;
 }
