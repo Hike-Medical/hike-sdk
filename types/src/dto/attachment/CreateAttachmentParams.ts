60da4bdf443be97d3ba84e2e25a1ae058377877a
gabriel-fishman-hike-medical <gabriel.fishman@hikemedical.com>
2025-09-19 15:14:32 -0400

    evaluationattachment should have multiple types (#864)
    * evaluationattachment should have multiple types
    
    * add additional attachment types
    
    * workflow->workflow links

--- types/src/dto/attachment/CreateAttachmentParams.ts
+++ types/src/dto/attachment/CreateAttachmentParams.ts
@@ -3,6 +3,6 @@ import { EvaluationAttachmentType } from '../../../prisma';
 export interface CreateAttachmentParams {
   name: string;
   tags?: Record<string, string>;
-  attachmentType: EvaluationAttachmentType;
+  attachmentTypes: EvaluationAttachmentType[];
   extension: string;
 }
