af25bf5791352a37a55364a39d652175067b5167
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2025-09-22 17:35:32 -0400

    remove deprecated medicare code (#869)

--- types/src/dto/attachment/UpdateAttachmentParams.ts
+++
@@ -1,7 +0,0 @@
-import { EvaluationAttachmentStatus, Prisma } from '../../../prisma';
-
-export interface UpdateAttachmentParams {
-  status: EvaluationAttachmentStatus;
-  metadata?: Prisma.JsonValue;
-  companyId: string;
-}
