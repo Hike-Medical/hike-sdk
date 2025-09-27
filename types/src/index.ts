2e0cc0d58b3607860833eca9d4c0843b439253ce
Gabriel Fishman <gabriel.fishman@hikemedical.com>
2025-09-26 20:19:17 -0400

    changes for refactoring workflows to make the data errors stateless

--- types/src/index.ts
+++ types/src/index.ts
@@ -224,7 +224,6 @@ export * from './dto/workflow/Attachment';
 export * from './dto/workflow/SearchWorkflowsParams';
 export * from './dto/workflow/WorkflowAttachment';
 export * from './dto/workflow/WorkflowDto';
-export * from './dto/workflow/WorkflowError';
 export * from './dto/workflow/WorkflowFactsResult';
 export * from './dto/workflow/WorkflowSearchResult';
 export * from './dto/workflow/WorkflowStatus';
