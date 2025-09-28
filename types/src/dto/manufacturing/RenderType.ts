cee4d5e336aff2e2b9abcf7e172dc11488dddacc
Basem <basem@hikemedical.com>
2025-09-29 07:56:58 +0800

    Convert enum to object literal due to Swagger error (#884)

--- types/src/dto/manufacturing/RenderType.ts
+++ types/src/dto/manufacturing/RenderType.ts
@@ -1,7 +1,9 @@
-export enum RenderType {
-  LEFT_ONLY = 1,
-  LEFT_MIRROR = 3,
-  RIGHT_ONLY = 2,
-  RIGHT_MIRROR = 4,
-  BOTH = 0
-}
+export const RenderType = {
+  LEFT_ONLY: 1,
+  LEFT_MIRROR: 3,
+  RIGHT_ONLY: 2,
+  RIGHT_MIRROR: 4,
+  BOTH: 0
+} as const;
+
+export type RenderType = (typeof RenderType)[keyof typeof RenderType];
