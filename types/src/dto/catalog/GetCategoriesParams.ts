a402c27817ada651d72c67b5421ce7246fdbdca4
Basem Hike <basem@hikemedical.com>
2025-10-02 13:45:02 +0800

    Add manufacture to category filter

--- types/src/dto/catalog/GetCategoriesParams.ts
+++ types/src/dto/catalog/GetCategoriesParams.ts
@@ -2,9 +2,10 @@ import { Prisma } from '../../../prisma';
 import type { PagedParams } from '../PagedParams';
 
 export interface GetCategoriesParams extends PagedParams {
+  term?: string;
   parentId?: string | null;
   supplierId?: string;
-  term?: string;
+  manufacturerId?: string;
   sortBy?: 'name' | 'parentId' | 'createdAt' | 'updatedAt';
   sortOrder?: Prisma.SortOrder;
 }
