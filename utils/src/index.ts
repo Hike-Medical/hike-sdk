d32c8598070a545ca3d383ece6673fa976457c04
gabriel-fishman-hike-medical <gabriel.fishman@hikemedical.com>
2025-10-02 13:33:31 -0400

    refactor facts registry so there is a single source of truth for schemas (#896)
    * refactor facts so there is a single source of truth
    
    * add capability to hide certain facts in the UX
    
    * add transform functons and validation to facts

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -6,7 +6,6 @@ export * from './converters/formatCamelCase';
 export * from './converters/formatConstant';
 export * from './converters/formatCurrency';
 export * from './converters/formatDate';
-export * from './converters/formatPhoneNumber';
 export * from './converters/formatRenderType';
 export * from './converters/parseCityStateZip';
 export * from './converters/parseClinicianName';
