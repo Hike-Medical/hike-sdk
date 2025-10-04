1c4feca8d46d748913366256f0ee7daf6a734e24
Basem Hike <basem@hikemedical.com>
2025-10-04 13:39:17 +0800

    Update

--- types/src/dto/notification/GetEnrollPatientsNotificationJobsParams.ts
+++ types/src/dto/notification/GetEnrollPatientsNotificationJobsParams.ts
@@ -1,3 +1,3 @@
 import type { PagedParams } from '../PagedParams';
 
-export interface GetEnrollPatientsNotificationJobsParams extends PagedParams {}
+export interface GetEnrollPatientsNotificationJobsParams extends Omit<PagedParams, 'sortBy'> {}
