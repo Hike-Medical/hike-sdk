78f8a10ccae778f91c5c80e8b3f5c4133a2e0452
nvarsh-hm <nik@hikemedical.com>
2025-10-06 13:34:35 -0400

    fix: types

---
+++ ui/src/hooks/workflows/useCreateWorkflowWithFile.ts
@@ -0,0 +1,28 @@
+import { createWorkflowWithFile } from '@hike/services';
+import { HikeError, WorkflowDto } from '@hike/types';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+
+interface CreateWorkflowWithFileParams {
+  workflowName: string;
+  attachment: {
+    name: string;
+    bucket: string;
+    key: string;
+    region: string;
+    types: string[];
+  };
+  externalPatientId?: string;
+  externalEvaluationId?: string;
+}
+
+export const useCreateWorkflowWithFile = (
+  options?: Omit<
+    UseMutationOptions<WorkflowDto, HikeError<null>, CreateWorkflowWithFileParams>,
+    'mutationKey' | 'mutationFn'
+  >
+) =>
+  useMutation({
+    mutationKey: ['createWorkflowWithFile'],
+    mutationFn: (data) => createWorkflowWithFile(data),
+    ...options
+  });
