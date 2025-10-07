594f45116753e332af8575d3f76f959f4a28cd92
nvarsh-hm <nik@hikemedical.com>
2025-10-06 20:14:06 -0400

    fix: remove unused query params

--- ui/src/hooks/workflows/useWorkflowLogs.ts
+++ ui/src/hooks/workflows/useWorkflowLogs.ts
@@ -5,8 +5,6 @@ import { useQuery } from '@tanstack/react-query';
 export const useWorkflowLogs = (workflowId: string) => {
   return useQuery<WorkflowLogDto[]>({
     queryKey: ['workflow-logs', workflowId],
-    queryFn: () => getWorkflowLogs(workflowId),
-    enabled: !!workflowId,
-    retry: false
+    queryFn: () => getWorkflowLogs(workflowId)
   });
 };
