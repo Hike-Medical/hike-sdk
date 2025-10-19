55431dbfac7ca96bd6763b1a48929e0e795aa922
Basem Hike <basem@hikemedical.com>
2025-10-19 17:19:48 +0800

    Updates

--- types/src/dto/catalog/ImportCatalogProductsResponse.ts
+++ types/src/dto/catalog/ImportCatalogProductsResponse.ts
@@ -1,12 +1,18 @@
 export interface ImportCatalogProductsResponse {
   productsParsed: number;
   productsProcessed: number;
+  productsCreated: number;
+  productsUpdated: number;
   productsSkipped: number;
   variantsParsed: number;
   variantsProcessed: number;
+  variantsCreated: number;
+  variantsUpdated: number;
   variantsSkipped: number;
   attributesParsed: number;
   attributesProcessed: number;
+  attributesCreated: number;
+  attributesUpdated: number;
   attributesSkipped: number;
   manufacturersCreated: string[];
   parsingErrors: string[];
