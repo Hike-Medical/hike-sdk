8a0c1f46547e881e12f8694c68355a2f2744b980
Basem Hike <basem@hikemedical.com>
2025-10-19 20:59:31 +0800

    Update

--- types/src/dto/catalog/GetProductsParams.ts
+++ types/src/dto/catalog/GetProductsParams.ts
@@ -2,15 +2,24 @@ import { Prisma } from '../../../prisma';
 import type { PagedParams } from '../PagedParams';
 
 export interface GetProductsParams extends PagedParams {
-  filter?: Partial<
-    Record<
-      'term' | 'categoryId' | 'manufacturerId' | 'supplierId' | 'deviceTypeId' | 'billingCode' | 'active' | 'favorite',
-      string
-    >
-  >;
+  term?: string;
   minPrice?: number;
   maxPrice?: number;
   includeVariants?: boolean;
+  filter?: Partial<
+    Record<
+      | 'name'
+      | 'sku'
+      | 'categoryId'
+      | 'manufacturerId'
+      | 'supplierId'
+      | 'deviceTypeId'
+      | 'billingCode'
+      | 'active'
+      | 'favorite',
+      string
+    >
+  >;
   sortBy?: 'name' | 'sku' | 'price' | 'favorite' | 'createdAt' | 'updatedAt';
   sortOrder?: Prisma.SortOrder;
 }
