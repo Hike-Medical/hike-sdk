12ed7767bdcc7d88934a871709df4c7247ed2ddc
Basem <basem@hikemedical.com>
2025-10-20 20:41:23 +0800

    Add batching to notification enrollments (#905)
    * Add batching to notification enrollments
    
    * Updates
    
    * Updates
    
    * Update
    
    * Update
    
    * Update
    
    * Updates
    
    * Update
    
    * Update
    
    * Updates

---
+++ types/src/dto/notification/NotificationProgress.ts
@@ -0,0 +1,23 @@
+export interface NotificationProgress {
+  rootJobId: string;
+  messageId: string;
+  totalPatients: number;
+  processed: number;
+  isActive: boolean;
+  progress: number; // 0-1, overall progress of all batches
+  totalBatches: number;
+  completedBatches: number;
+  remainingBatches: number;
+  failedBatches: number;
+  currentBatch?: {
+    batchIndex: number;
+    batchOffset: number;
+    batchSize: number;
+    patientsInBatch: number;
+    isProcessing: boolean;
+    state: string;
+    startsAt?: number; // Unix timestamp (ms) when delayed batch will start
+  };
+  startedAt?: number | null;
+  finishedAt?: number | null;
+}
