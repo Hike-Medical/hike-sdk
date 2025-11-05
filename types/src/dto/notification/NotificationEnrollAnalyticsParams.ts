25c93b967c8325156bd8996022c3627b2f2920f4
nvarsh-hm <nik@hikemedical.com>
2025-11-04 22:43:01 -0500

    feat: add tri-state

--- types/src/dto/notification/NotificationEnrollAnalyticsParams.ts
+++ types/src/dto/notification/NotificationEnrollAnalyticsParams.ts
@@ -3,10 +3,12 @@ import type { PagedParams } from '../PagedParams';
 export interface NotificationEnrollAnalyticsParams extends PagedParams {
   status: 'ACTIVE' | 'INACTIVE';
   search?: string;
+  opened?: 'opened' | 'unopened' | 'undefined';
 }
 
 export interface ExportNotificationEnrollAnalyticsParams {
   status: 'ACTIVE' | 'INACTIVE';
   search?: string;
   patientIds?: string[];
+  opened?: 'opened' | 'unopened' | 'undefined';
 }
