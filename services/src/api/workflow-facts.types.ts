6116100bdb538cf45c7628910d5294faefa63e01
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2025-11-05 14:40:01 -0500

    Amrith/connect workflow to insoles (#981)
    * update prisma to connect insoles
    
    * update facts
    
    * add selection visit
    
    * fix build errors
    
    * update medicare procurement only
    
    * add connected workflog
    
    * add evaluatoin types
    
    * add billing info and validate dates
    
    * add constant

--- services/src/api/workflow-facts.types.ts
+++ services/src/api/workflow-facts.types.ts
@@ -684,9 +684,9 @@ export const FactRegistry = {
   },
 
   // Operations
-  'ops.delivery.date': {
-    displayName: 'Delivery Date',
-    description: 'Date when the diabetic footwear was delivered',
+  'fitter.billing.date': {
+    displayName: 'Billing Date',
+    description: 'Date when the diabetic footwear should be billed',
     category: 'Operations',
     required: false,
     schema: dateISO,
@@ -796,12 +796,21 @@ export const FactRegistry = {
   },
 
   // Other
-  'fitter.clinic_name': {
-    displayName: 'Fitter Clinic Name',
-    description: 'Name of the clinic where the footwear was fitted',
+  'fitter.selection.visit.date': {
+    displayName: 'Fitter Selection Visit Date',
+    description: 'Date of the visit where the footwear was selected',
     category: 'Other',
     required: false,
-    schema: z.string().min(1),
+    schema: dateISO,
+    metadata: defaultMetadataSchema
+  },
+
+  'fitter.delivery.visit.date': {
+    displayName: 'Fitter Delivery Visit Date',
+    description: 'Date of the visit where the footwear was delivered',
+    category: 'Other',
+    required: false,
+    schema: dateISO,
     metadata: defaultMetadataSchema
   },
 
