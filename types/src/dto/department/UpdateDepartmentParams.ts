2a6a74e23f65b827b1a90eacfb97e5b9e5702412
Basem Emara <basem@emara.io>
2025-11-05 13:58:16 +0800

    Add departments services

---
+++ types/src/dto/department/UpdateDepartmentParams.ts
@@ -0,0 +1,3 @@
+export interface UpdateDepartmentParams {
+  name: string;
+}
--- ui/src/hooks/departments/useDepartments.ts
+++ ui/src/hooks/departments/useDepartments.ts
@@ -1,14 +1,16 @@
 import { fetchDepartments } from '@hike/services';
-import { Department, HikeError } from '@hike/types';
+import { DepartmentExtended, GetDepartmentsParams, HikeError } from '@hike/types';
 import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
 
-interface UseDepartmentsOptions extends Omit<UseQueryOptions<Department[], HikeError<null>>, 'queryKey' | 'queryFn'> {
+interface DepartmentsRequest
+  extends Omit<UseQueryOptions<DepartmentExtended[], HikeError<null>>, 'queryKey' | 'queryFn'> {
+  params?: GetDepartmentsParams;
   queryKey?: QueryKey;
 }
 
-export const useDepartments = ({ queryKey = [], ...options }: UseDepartmentsOptions = {}) =>
+export const useDepartments = ({ queryKey = [], params, ...options }: DepartmentsRequest = {}) =>
   useQuery({
-    queryKey: ['departments', queryKey],
-    queryFn: async () => await fetchDepartments(),
+    queryKey: ['departments', params, queryKey],
+    queryFn: async () => await fetchDepartments(params),
     ...options
   });
