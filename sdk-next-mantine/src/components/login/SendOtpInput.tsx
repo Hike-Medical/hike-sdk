de27f5999d20eb966fa686c742c995a2d8df9c79
Basem Emara <basem@emara.io>
2025-11-24 23:49:36 +0800

    Update

--- sdk-next-mantine/src/components/login/SendOtpInput.tsx
+++ sdk-next-mantine/src/components/login/SendOtpInput.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { ContactType, HikeErrorCode, toErrorMessage, VerifyInvitationResponse } from '@hike/sdk';
-import { useSendOtp, useVerifyInvitation } from '@hike/ui';
+import { useSendOtp } from '@hike/sdk-next';
 import { Alert, Anchor, Button, PinInput, Stack, Text, ThemeIcon } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import { IconExclamationCircleFilled } from '@tabler/icons-react';
 import { useTranslations } from 'next-intl';
-import { ComponentType, PropsWithChildren, ReactNode, useEffect, useRef, useState } from 'react';
+import { ComponentType, PropsWithChildren, ReactNode } from 'react';
 
 export interface SendOtpInputProps {
   contact: string;
@@ -30,71 +30,20 @@ export const SendOtpInput = ({
   onContactSupport,
   HikeShell
 }: SendOtpInputProps) => {
-  const [inputOtp, setInputOtp] = useState<string | null>(null);
-  const [countdown, setCountdown] = useState(0);
-  const [isCountdownShown, setIsCountdownShown] = useState(false);
-  const [isInvalidOtp, setIsInvalidOtp] = useState(false);
-  const [isVerified, setIsVerified] = useState(false);
-  const [unrecoverableError, setUnrecoverableError] = useState<HikeErrorCode | null>(null);
-  const hasSentInitialOtpRef = useRef(false);
   const tShared = useTranslations('shared');
   const t = useTranslations('shared.login.otp');
 
-  const { mutate: sendOtp, isPending: isSendingOtp } = useSendOtp({
-    onMutate: () => setIsInvalidOtp(false),
-    onSuccess: () => {
-      setInputOtp(null);
-      setIsCountdownShown(false);
-      setCountdown(30);
-    },
+  const { handleOtpChange, handleResendOtp, state, isSending, isVerifying } = useSendOtp({
+    contact,
+    contactType,
+    onVerified,
     onError: (error) => {
-      switch (error.errorCode) {
-        case HikeErrorCode.ERR_PATIENT_SIGNUP_NOT_ALLOWED:
-        case HikeErrorCode.ERR_AUTH_FORBIDDEN:
-          if (error.errorCode) {
-            setUnrecoverableError(error.errorCode);
-          }
-          return;
-        default: {
-          const message = toErrorMessage(error, t('error.couldNotSend'));
-          showNotification({ title: tShared('error.title'), message, color: 'red' });
-          setIsInvalidOtp(true);
-        }
-      }
+      const message = toErrorMessage(error, t('error.couldNotSend'));
+      showNotification({ title: tShared('error.title'), message, color: 'red' });
     }
   });
 
-  const { mutate: verifyInvitation, isPending: isVerifyingOtp } = useVerifyInvitation({
-    onMutate: () => setIsInvalidOtp(false),
-    onSuccess: async (response) => {
-      setCountdown(0);
-      setIsVerified(true);
-      await onVerified(response, inputOtp ?? '');
-    },
-    onError: () => setIsInvalidOtp(true)
-  });
-
-  useEffect(() => {
-    if (countdown > 0) {
-      const timer = setTimeout(() => setCountdown(countdown - 1), 1000);
-      return () => clearTimeout(timer);
-    }
-
-    return () => {};
-  }, [countdown]);
-
-  useEffect(() => {
-    if (!contact || !contactType || hasSentInitialOtpRef.current) {
-      return;
-    }
-
-    hasSentInitialOtpRef.current = true;
-
-    // Must execute on next runloop or stuck in pending state
-    setTimeout(() => sendOtp({ contact, contactType }), 0);
-  }, [contact, contactType, sendOtp]);
-
-  switch (unrecoverableError) {
+  switch (state.unrecoverableError) {
     case HikeErrorCode.ERR_PATIENT_SIGNUP_NOT_ALLOWED:
       return (
         <>
@@ -186,15 +135,8 @@ export const SendOtpInput = ({
         })}
       >
         <PinInput
-          value={inputOtp ?? ''}
-          onChange={(value) => {
-            setIsInvalidOtp(false);
-            setInputOtp(value);
-
-            if (value.length === 6) {
-              verifyInvitation(value);
-            }
-          }}
+          value={state.inputOtp ?? ''}
+          onChange={handleOtpChange}
           length={6}
           inputMode="numeric"
           placeholder=""
@@ -204,34 +146,30 @@ export const SendOtpInput = ({
           aria-label="One time code"
           oneTimeCode
           autoFocus
-          error={isInvalidOtp}
-          disabled={isVerifyingOtp || isVerified}
+          error={state.isInvalidOtp}
+          disabled={isVerifying || state.isVerified}
         />
-        {isInvalidOtp && (
+        {state.isInvalidOtp && (
           <Text fz="12" fw="bold" fs="italic" c="hike-danger" mx="auto">
             {t('invalidCode')}
           </Text>
         )}
       </HikeShell.Main>
       <HikeShell.Footer vertical>
-        {countdown > 0 && isCountdownShown ? (
+        {state.countdown > 0 && state.isCountdownShown ? (
           <Text fz="11" c="hike-danger" mx="auto">
-            {t('waitMessage', { seconds: countdown })}
+            {t('waitMessage', { seconds: state.countdown })}
           </Text>
         ) : (
           onSkipped &&
-          !isSendingOtp &&
-          !isVerifyingOtp && (
+          !isSending &&
+          !isVerifying && (
             <Button variant="subtle" onClick={onSkipped}>
               {tShared('action.skip')}
             </Button>
           )
         )}
-        <Button
-          variant="footer"
-          onClick={() => (countdown > 0 ? setIsCountdownShown(true) : sendOtp({ contact, contactType }))}
-          loading={isSendingOtp || isVerifyingOtp || isVerified}
-        >
+        <Button variant="footer" onClick={handleResendOtp} loading={isSending || isVerifying || state.isVerified}>
           {t('resendButton')}
         </Button>
       </HikeShell.Footer>
