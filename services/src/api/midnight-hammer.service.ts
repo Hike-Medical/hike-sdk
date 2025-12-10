204bff1f3e1af55cb5058fb74a86562babe62439
nvarsh-hm <nik@hikemedical.com>
2025-12-10 12:35:25 -0500

    feaT: pmh analytics

---
+++ services/src/api/midnight-hammer.service.ts
@@ -0,0 +1,13 @@
+import { GetProjectMidnightHammerAnalyticsParams, ProjectMidnightHammerAnalyticsResponse } from '@hike/types';
+import { backendApi } from '../utils/backendApi';
+
+export const midnightHammerService = {
+  getAnalytics: async (
+    params?: GetProjectMidnightHammerAnalyticsParams
+  ): Promise<ProjectMidnightHammerAnalyticsResponse> => {
+    const response = await backendApi.get<ProjectMidnightHammerAnalyticsResponse>('/midnight-hammer/analytics', {
+      params
+    });
+    return response.data;
+  }
+};
