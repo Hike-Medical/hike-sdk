927674af18ca16f986a0c3b888315ab5e617971d
Amrith Balachander <amrith@hikemedical.com>
2025-12-12 13:59:31 -0600

    update past tense station params

--- types/src/dto/workbench/GetPastTenseStationsParams.ts
+++ types/src/dto/workbench/GetPastTenseStationsParams.ts
@@ -3,4 +3,5 @@ import { PagedParams } from '../PagedParams';
 export interface GetPastTenseStationsParams extends PagedParams {
   previousStatus: string;
   searchQuery?: string;
+  laneId?: string;
 }
