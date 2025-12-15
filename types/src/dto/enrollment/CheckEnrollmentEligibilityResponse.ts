b9ed28436a67edd903eb7f5bbcc61d7be022226a
Basem <basem@hikemedical.com>
2025-12-15 15:48:11 -0500

    Enrollment eligibility via Stedi (#1098)

--- types/src/dto/enrollment/CheckEnrollmentEligibilityResponse.ts
+++ types/src/dto/enrollment/CheckEnrollmentEligibilityResponse.ts
@@ -2,5 +2,10 @@ import type { EligibilityReason } from './EligibilityReason';
 
 export interface CheckEnrollmentEligibilityResponse {
   isEligible: boolean;
+  stedi?: {
+    controlNumber?: string;
+    hasActiveCoverage?: boolean;
+    summary?: string;
+  };
   reason: EligibilityReason;
 }
