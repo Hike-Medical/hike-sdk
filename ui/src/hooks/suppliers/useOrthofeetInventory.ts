afc51e4ade811c2032840a5c573f62d91c82568b
Basem <basem@hikemedical.com>
2025-12-16 16:28:22 -0500

    Update warehouse inventory service (#1108)

--- ui/src/hooks/suppliers/useOrthofeetInventory.ts
+++ ui/src/hooks/suppliers/useOrthofeetInventory.ts
@@ -1,16 +1,16 @@
-import { fetchOrthofeetInventoryBySku } from '@hike/services';
+import { fetchOrthofeetInventory } from '@hike/services';
 import { GetOrthofeetInventoryResponse, HikeError } from '@hike/types';
 import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query';
 
 interface UseOrthofeetInventoryOptions
   extends Omit<UseQueryOptions<GetOrthofeetInventoryResponse, HikeError<null>>, 'queryKey' | 'queryFn'> {
-  sku: string;
+  sku: string | string[];
   queryKey?: QueryKey;
 }
 
-export const useOrthofeetInventoryBySku = ({ sku, queryKey = [], ...options }: UseOrthofeetInventoryOptions) =>
+export const useOrthofeetInventory = ({ sku, queryKey = [], ...options }: UseOrthofeetInventoryOptions) =>
   useQuery({
-    queryKey: ['orthofeetInventoryBySku', sku, queryKey],
-    queryFn: async () => await fetchOrthofeetInventoryBySku(sku),
+    queryKey: ['orthofeetInventory', sku, queryKey],
+    queryFn: async () => await fetchOrthofeetInventory(sku),
     ...options
   });
