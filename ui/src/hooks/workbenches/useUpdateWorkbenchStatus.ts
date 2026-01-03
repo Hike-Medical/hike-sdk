2c47b82035b74aec5de702dde2eb877a24c8c945
Amrith Balachander <amrith@hikemedical.com>
2026-01-03 13:04:40 -0500

    update lane and workbench management

---
+++ ui/src/hooks/workbenches/useUpdateWorkbenchStatus.ts
@@ -0,0 +1,20 @@
+import { updateWorkbenchStatus } from '@hike/services';
+import { HikeError, Workbench, WorkbenchStatus } from '@hike/types';
+import { UseMutationOptions, useMutation } from '@tanstack/react-query';
+
+interface UpdateWorkbenchStatusContext {
+  workbenchId: string;
+  status: WorkbenchStatus;
+  companyIds?: string[];
+}
+
+export const useUpdateWorkbenchStatus = (
+  options?: UseMutationOptions<Workbench, HikeError<null>, UpdateWorkbenchStatusContext>
+) =>
+  useMutation({
+    mutationKey: ['updateWorkbenchStatus'],
+    mutationFn: async ({ workbenchId, status, companyIds }) =>
+      await updateWorkbenchStatus(workbenchId, status, companyIds),
+    ...options
+  });
+
