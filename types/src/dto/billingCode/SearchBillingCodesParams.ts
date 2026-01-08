4e51620cc598619feb10b6dcf8002a745b6451ab
Amrith Balachander <amrith@hikemedical.com>
2026-01-08 17:07:25 -0500

    add favorite for billing codes

--- types/src/dto/billingCode/SearchBillingCodesParams.ts
+++ types/src/dto/billingCode/SearchBillingCodesParams.ts
@@ -2,7 +2,8 @@ import { Prisma } from '../../../prisma';
 import type { PagedParams } from '../PagedParams';
 
 export interface SearchBillingCodesParams extends PagedParams {
-  term: string;
+  term?: string;
+  isFavorite?: boolean;
   sortBy?: 'id' | 'summary' | 'description';
   sortOrder?: Prisma.SortOrder;
 }
--- ui/src/hooks/billing-codes/useBillingCodesSearch.ts
+++ ui/src/hooks/billing-codes/useBillingCodesSearch.ts
@@ -5,15 +5,31 @@ import { QueryKey, UseQueryOptions, useQuery } from '@tanstack/react-query';
 interface UseBillingCodesSearchOptions
   extends Omit<UseQueryOptions<PagedResponse<BillingCode[]> | null, HikeError<null>>, 'queryKey' | 'queryFn'> {
   searchTerm?: string | null;
+  isFavorite?: boolean;
   queryKey?: QueryKey;
 }
 
-export const useBillingCodesSearch = ({ searchTerm, queryKey = [], ...options }: UseBillingCodesSearchOptions) => {
-  const key = ['useBillingCodesSearch', ...(searchTerm ? [searchTerm] : []), ...queryKey];
+export const useBillingCodesSearch = ({
+  searchTerm,
+  isFavorite,
+  queryKey = [],
+  ...options
+}: UseBillingCodesSearchOptions) => {
+  const key = [
+    'useBillingCodesSearch',
+    ...(searchTerm ? [searchTerm] : []),
+    ...(isFavorite ? ['favorite'] : []),
+    ...queryKey
+  ];
 
   const query = useQuery({
     queryKey: key,
-    queryFn: async () => (searchTerm ? await searchBillingCodes({ term: searchTerm }) : await fetchBillingCodes()),
+    queryFn: async () => {
+      if (searchTerm || isFavorite !== undefined) {
+        return await searchBillingCodes({ term: searchTerm ?? '', isFavorite });
+      }
+      return await fetchBillingCodes();
+    },
     ...options
   });
 
