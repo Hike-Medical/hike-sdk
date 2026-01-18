c6b8673cc65454aae1d373606c5cdedfa26e68e1
Amrith Balachander <amrith@hikemedical.com>
2026-01-18 12:31:53 -0500

    clean up unused code

--- types/src/dto/analytics/CompanyTurnaroundStats.ts
+++
@@ -1,7 +0,0 @@
-interface CompanyTurnaroundStats {
-  companyName: string | null;
-  turnaroundDays: number | null;
-  deliveryDiffDays: number | null;
-}
-
-export type CompanyTurnaroundStatsResponse = CompanyTurnaroundStats[];
