ca97feeff811bce842fe6d7ee39a541d8f6a336a
AmrithHike <139060983+AmrithHike@users.noreply.github.com>
2026-01-30 19:49:29 -0500

    update project T (#1261)

--- types/src/dto/workbench/GetNeedsValidationWorkbenchParams.ts
+++ types/src/dto/workbench/GetNeedsValidationWorkbenchParams.ts
@@ -2,4 +2,6 @@ import { GetAggregatedParams } from './GetAggregatedParams';
 
 export interface GetNeedsValidationWorkbenchParams extends GetAggregatedParams {
   prioritizeRushOrders?: boolean;
+  /** Filter by whether assets have a taikaId (true = has taikaId, false = no taikaId, undefined = no filter) */
+  hasTaikaId?: boolean;
 }
