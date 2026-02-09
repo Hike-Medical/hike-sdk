0f8aa2d23846c2f3a9263f28fda3f5d4ba96af0b
nvarsh-hm <nik@hikemedical.com>
2026-02-09 13:55:46 -0500

    fix: add appIds into const

--- types/src/config/AppId.ts
+++ types/src/config/AppId.ts
@@ -1,6 +1,9 @@
-export type AppId =
-  | '@hike/admin-web'
-  | '@hike/insoles-web'
-  | '@hike/consumer-web'
-  | '@hike/stepzero-web'
-  | '@hike/backend';
+export const appIds = [
+  '@hike/admin-web',
+  '@hike/insoles-web',
+  '@hike/consumer-web',
+  '@hike/stepzero-web',
+  '@hike/backend'
+] as const;
+
+export type AppId = (typeof appIds)[number];
