a16c4b92d0c0e9587a7a7f457ce09d706f46ec9f
Charles <charles.lussier@hikemedical.com>
2026-02-17 13:18:46 -0500

    feat: require operator to enter package weight before printing shipping label

--- types/src/dto/shipping/CompletePackingJob.ts
+++ types/src/dto/shipping/CompletePackingJob.ts
@@ -2,6 +2,7 @@ export interface CompletePackingJobBody {
   configurationId: string;
   orderIds: string[];
   shippingPackageId: string;
+  weight: number;
 }
 
 export interface CompletePackingJobResponse {
