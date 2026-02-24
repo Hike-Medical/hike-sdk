863d4bc8bd2a09b11a278b5234edd9c71f25e387
Nakib Momin <nbmomin14@gmail.com>
2026-02-24 14:25:55 -0500

    added helper for downloading file

---
+++ utils/src/helpers/downloadFile.ts
@@ -0,0 +1,18 @@
+export const downloadFile = (data: BlobPart | BlobPart[], filename: string, type: string, revokeTimeoutMs = 100) => {
+  if (typeof window === 'undefined' || typeof document === 'undefined') {
+    return;
+  }
+
+  const blobData = Array.isArray(data) ? data : [data];
+  const blob = new Blob(blobData, { type });
+  const url = window.URL.createObjectURL(blob);
+  const anchorElement = window.document.createElement('a');
+
+  anchorElement.href = url;
+  anchorElement.download = filename;
+  window.document.body.appendChild(anchorElement);
+  anchorElement.click();
+  anchorElement.remove();
+
+  setTimeout(() => window.URL.revokeObjectURL(url), revokeTimeoutMs);
+};
